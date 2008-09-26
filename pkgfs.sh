084d6664b21c9e2a38a947fe554cd7a91f825bce
Juan RP <xtraeme@gmail.com>
2008-09-26 23:23:41 +0200

    Remove the list target... it's not very useful.
    Also use my copyright for this file.
    
    --HG--
    extra : convert_revision : 75a3b2fc4aa7785437e4f3de53b23dd64683d990

--- pkgfs.sh
+++ pkgfs.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
 #
+#-
+# Copyright (c) 2008 Juan Romero Pardines.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#-
+#
 # TODO
 # 	Multiple distfiles in a package.
-#	Support GNU/BSD-makefile style only packages.
+#	Support GNU/BSD-makefile style source distribution files.
 # 	Actually do the symlink dance (stow/unstow).
-#	Implement listing packages in PKGFS_DISTDIR.
+#
 #
 # Default path to configuration file, can be overriden
 # via the environment or command line.
@@ -29,7 +54,6 @@ $_progname: [-cef] <target> <file>
 Targets
 	build	Build package from <file>.
 	info	Show information about <file>.
-	list	List packages installed in PKGFS_DESTDIR.
 
 Options
 	-c	Path to global configuration file.
@@ -285,6 +309,9 @@ build_pkg_from_source()
 	build_pkg
 }
 
+#
+# main()
+#
 args=$(getopt c:ef $*)
 [ "$?" -ne 0 ] && usage
 
@@ -352,9 +379,6 @@ build)
 	;;
 info)
 	show_info_from_buildfile
-	;;
-list)
-	list_installed_packages
 	;;
 *)
 	echo "*** ERROR invalid target '${_target}' ***"
