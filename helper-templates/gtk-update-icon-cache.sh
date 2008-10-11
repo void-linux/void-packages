67674608580605b8c921048ced3a6f44d46ef0e1
Juan RP <xtraeme@gmail.com>
2008-10-11 02:03:45 +0200

    Added hicolor-icon-theme-0.10 and its acompannion helper.
    --HG--
    extra : convert_revision : 885f1c2f7759caa86909aed1831522d44b1e159f

---
+++ helper-templates/gtk-update-icon-cache.sh
@@ -0,0 +1,14 @@
+#
+# This helper updates the GTK's theme icon cache as requested by
+# any template.
+#
+
+gtkupdate_iconcache_cmd=$PKGFS_MASTERDIR/bin/gtk-update-icon-cache
+iconcache_theme_dir=$PKGFS_MASTERDIR/share/icons/hicolor
+
+if [ -x $gtkupdate_iconcache_cmd -a -d $iconcache_theme_dir ]; then
+	$gtkupdate_iconcache_cmd -f -t $iconcache_theme_dir && \
+		echo "=> Updated GTK's hicolor icon cache theme."
+fi
+
+unset gtkupdate_iconcache_cmd iconcache_theme_dir
