cd85d4060838f2107cd77894f713df266481172e
Juan RP <xtraeme@gmail.com>
2008-10-15 05:57:27 +0200

    Fix paths in pkg-config files from mono and gtk-sharp.
    --HG--
    extra : convert_revision : 075ecd8ba9dca2e2fc7e05c6da64ef269a74aa47

--- templates/gtk-sharp-runstuff-before-build.sh
+++ templates/gtk-sharp-runstuff-before-build.sh
@@ -6,3 +6,14 @@
 
 replace_interpreter perl parser/gapi2xml.pl
 replace_interpreter perl parser/gapi_pp.pl
+
+#
+# Fix up pkg-config files.
+gtksharp_pc_files="gtk/gtk-sharp-2.0.pc.in gtkdotnet/gtk-dotnet-2.0.pc.in
+ glib/glib-sharp-2.0.pc.in parser/gapi-2.0.pc.in glade/glade-sharp-2.0.pc.in"
+
+for f in ${gtksharp_pc_files}; do
+	$sed_cmd -e "s|\${pcfiledir}/../..|$XBPS_MASTERDIR|g"   \
+		$wrksrc/$f > $wrksrc/$f.sed &&  \
+		$mv_cmd $wrksrc/$f.sed $wrksrc/$f
+done
--- templates/mono-runstuff-before-configure.sh
+++ templates/mono-runstuff-before-configure.sh
@@ -1,10 +1,10 @@
 #
-# Replaces hardcoded shebang files in some scripts.
-#
 # Perl files with hardcoded shebang path.
+#
 mono_perl_files="mcs/errors/do-tests.pl mcs/tools/scan-tests.pl
 msvc/create-windef.pl mono/benchmark/test-driver mono/cil/make-opcodes-def.pl
 mono/metadata/make-bundle.pl mono/mini/genmdesc.pl mono/tests/stress-runner.pl"
+
 #
 # Bash files with hardcoded shebang path.
 #
@@ -29,3 +29,29 @@ done
 for f in ${mono_perl_files}; do
 	replace_interpreter perl $f
 done
+
+unset mono_perl_files mono_bash_files
+
+#
+# Fix up wrong pkg-config prefix vars.
+#
+mono_pc_files="data/cecil.pc.in data/dotnet.pc.in
+ data/dotnet35.pc.in data/mint.pc.in data/mono-cairo.pc.in
+ data/mono.pc.in data/smcs.pc.in scripts/mono-nunit.pc.in"
+
+for f in ${mono_pc_files}; do
+	$sed_cmd -e "s|\${pcfiledir}/../..|$XBPS_MASTERDIR|g"	\
+		$wrksrc/$f > $wrksrc/$f.sed &&  \
+		$mv_cmd $wrksrc/$f.sed $wrksrc/$f
+done
+
+unset mono_pc_files
+
+#
+# Fix up hardcoded default path in mcs.
+#
+$sed_cmd -e "s|/usr/local|$XBPS_MASTERDIR|g"	\
+	$wrksrc/mcs/build/config-default.make >	\
+	$wrksrc/mcs/build/config-default.make.in &&	\
+	$mv_cmd $wrksrc/mcs/build/config-default.make.in \
+		$wrksrc/mcs/build/config-default.make
