071a1148c53d237d51c50476be9eb99789e09b81
Juan RP <xtraeme@gmail.com>
2008-12-19 03:06:43 +0100

    Fix another stupid error in genindex.
    --HG--
    extra : convert_revision : 18b91a31fc42ec374a5f51d4c32fcc85d1615738

--- utils/sh/binpkg-genindex.sh
+++ utils/sh/binpkg-genindex.sh
@@ -92,7 +92,7 @@ write_repo_pkgindex_header()
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>xbps_available_packages/<key>
+	<key>xbps_available_packages</key>
 	<array>
 _EOF
 }
