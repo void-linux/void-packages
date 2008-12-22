0cd947bff0d34e7a98d80ba365391ae66c5fb181
Juan RP <xtraeme@gmail.com>
2008-12-22 05:29:26 +0100

    chroot.sh: don't hardcode path to xbps-src anymore.
    --HG--
    extra : convert_revision : bbaf586b398a1c359c8ebee085af7627ca9336e9

--- shutils/chroot.sh
+++ shutils/chroot.sh
@@ -165,7 +165,7 @@ xbps_chroot_handler()
 		[ -n "$only_destdir" ] && \
 			local lenv="install_destdir_target=yes"
 		env in_chroot=yes ${lenv} chroot $XBPS_MASTERDIR \
-			/xbps/xbps-src.sh $action $pkg
+			xbps-src $action $pkg
 	fi
 	msg_normal "Exiting from the chroot on $XBPS_MASTERDIR."
 	umount_chroot_fs
