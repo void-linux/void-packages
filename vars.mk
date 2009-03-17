b509e9eebf72c4ed058b500d381ca1ddc16db22b
Juan RP <xtraeme@gmail.com>
2009-03-17 16:30:44 +0100

    vars.mk: also set _BSD_SOURCE for rcorder.
    --HG--
    extra : convert_revision : af3e53086209f56f4b2a3bb14093e3f2571cc717

--- vars.mk
+++ vars.mk
@@ -8,7 +8,7 @@ TOPDIR	?= ..
 INSTALL_STRIPPED ?= -s
 
 LDFLAGS += -L$(TOPDIR)/lib -L$(PREFIX)/lib -lxbps
-CPPFLAGS += -I$(TOPDIR)/include -D_XOPEN_SOURCE=600
+CPPFLAGS += -I$(TOPDIR)/include -D_BSD_SOURCE -D_XOPEN_SOURCE=600
 WARNFLAGS ?= -pedantic -std=c99 -Wall -Wextra -Werror -Wshadow -Wformat=2
 WARNFLAGS += -Wmissing-declarations -Wcomment -Wunused-macros -Wendif-labels
 CFLAGS += $(WARNFLAGS) -O2 -fPIC -DPIC
