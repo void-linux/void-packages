3da96cb6e4d92ff6235e086f1ac836cbe32e5a4e
Juan RP <xtraeme@gmail.com>
2009-04-17 00:58:44 +0200

    Remove unused fontconfig/mkfontxx helpers.
    --HG--
    extra : convert_revision : e36bff58651d6261e9a34eaa862478578a619e3a

--- templates/helpers/fontconfig-update.sh
+++
@@ -1,9 +0,0 @@
-#
-# This helpers runs fc-cache after fontconfig has been installed,
-# and update its list of fonts.
-#
-
-if [ -x /usr/bin/fc-cache ]; then
-	/usr/bin/fc-cache -f
-	[ "$?" -eq 0 ] && echo "=> Updated fontconfig fonts cache."
-fi
