028415a297f727423af77b36a683730fe1371070
Juan RP <xtraeme@gmail.com>
2009-10-05 18:34:39 +0200

    xbps-base-files: fix /etc/profile.d/vim.sh. Bump to 0.26.
    --HG--
    extra : convert_revision : e3afc5e5cae73b65a5b6c7257a6d6cd423701bc2

--- templates/xbps-base-files/files/vim.sh
+++ templates/xbps-base-files/files/vim.sh
@@ -1,6 +1,6 @@
 if [ -n "$BASH_VERSION" -o -n "$KSH_VERSION" -o -n "$ZSH_VERSION" ]; then
-  [ -x //usr/bin/id ] || return
-  [ `//usr/bin/id -u` -le 100 ] && return
+  [ -x /bin/id ] || return
+  [ `/bin/id -u` -le 100 ] && return
   # for bash and zsh, only if no alias is already set
   alias vi >/dev/null 2>&1 || alias vi=vim
 fi
