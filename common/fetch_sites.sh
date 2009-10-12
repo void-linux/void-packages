f6bb96631580191087e21f2b3864e974e00be784
Juan RP <xtraeme@gmail.com>
2009-10-12 22:13:50 +0200

    Added common directory, where all .sh files will be sourced.
    Vars set in these files can be used by any build template.
    
    --HG--
    extra : convert_revision : 61c3d6fb168f9b48536e0addc5e4901e2b486c92

---
+++ common/fetch_sites.sh
@@ -0,0 +1,12 @@
+# 
+# HTTP/FTP vars that can be used by templates.
+#
+
+SOURCEFORGE_SITE="http://downloads.sourceforge.net/sourceforge"
+NONGNU_SITE="http://download.savannah.nongnu.org/releases"
+UBUNTU_SITE="http://archive.ubuntu.com/ubuntu/pool"
+XORG_SITE="http://xorg.freedesktop.org/releases/individual"
+DEBIAN_SITE="http://ftp.debian.org/debian/pool"
+GNOME_SITE="http://ftp.gnome.org/pub/GNOME/sources"
+KERNEL_SITE="http://www.kernel.org/pub/linux"
+CPAN_SITE="http://cpan.perl.org/modules/by-module"
