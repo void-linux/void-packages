0fe4e35138b1146decb946905690dbbddccd10be
Juan RP <xtraeme@gmail.com>
2009-10-18 09:25:16 +0200

    Added a helper for installing python modules.
    To use it "build_style" must be set to "python-module", by default
    --prefix=usr --root=$DESTDIR are passed as arguments to setup.py,
    these can be overriden by using "make_install_args".
    
    --HG--
    extra : convert_revision : 91b191d481dfff3c612da0621007ae1b1c7d33aa

---
+++ helpers/python-module.sh
@@ -0,0 +1,14 @@
+#
+# This helper is for templates installing python modules.
+#
+
+do_install()
+{
+	if [ -z "$make_install_args" ]; then
+		make_install_args="--prefix=usr --root=$DESTDIR"
+	fi
+
+	cd ${wrksrc} || return 1
+	sed -i 's|man/man1|share/man/man1|g' setup.py || return 1
+	python setup.py install ${make_install_args} || return 1
+}
