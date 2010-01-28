259d7f0ce7b8af5cd3756e027923f7bae174d404
Juan RP <xtraeme@gmail.com>
2010-01-28 19:05:03 +0100

    shutils/metadata.sh: if xbps-uhelper sanitize-plist fails, return error.
    --HG--
    extra : convert_revision : 949a19e83aff79731b2d315669114568fe4f81aa

--- xbps-src/shutils/metadata.sh
+++ xbps-src/shutils/metadata.sh
@@ -359,8 +359,8 @@ _EOF
 	mv -f $TMPFPLIST ${DESTDIR}/files.plist
 	mv -f $TMPFPROPS ${DESTDIR}/props.plist
 
-	$XBPS_PKGDB_CMD sanitize-plist ${DESTDIR}/files.plist
-	$XBPS_PKGDB_CMD sanitize-plist ${DESTDIR}/props.plist
+	$XBPS_PKGDB_CMD sanitize-plist ${DESTDIR}/files.plist || exit 1
+	$XBPS_PKGDB_CMD sanitize-plist ${DESTDIR}/props.plist || exit 1
 	chmod 644 ${DESTDIR}/files.plist ${DESTDIR}/props.plist
 	[ -f $metadir/flist ] && chmod 644 $metadir/flist
 
