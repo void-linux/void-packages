d5569aa9acfe776442b5ca112e798ace5b55c32a
Juan RP <xtraeme@gmail.com>
2012-01-29 11:54:34 +0100

    base-files: remove profile.d/vim.sh, conflicts with busybox vi.

--- srcpkgs/base-files/files/vim.sh
+++
@@ -1,6 +0,0 @@
-if [ -n "$BASH_VERSION" -o -n "$KSH_VERSION" -o -n "$ZSH_VERSION" ]; then
-  [ -x /bin/id ] || return
-  [ `/bin/id -u` -le 100 ] && return
-  # for bash and zsh, only if no alias is already set
-  alias vi >/dev/null 2>&1 || alias vi=vim
-fi
