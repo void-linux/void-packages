599ee6e2cb3801d0cf66902c8437d4d50cd2ed83
Juan RP <xtraeme@gmail.com>
2012-12-17 12:32:21 +0100

    New package: thunderbird-17.0.

---
+++ srcpkgs/thunderbird/files/vendor.js
@@ -0,0 +1,9 @@
+// Use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default mailer checking.
+pref("mail.shell.checkDefaultMail", false);
+
+// Don't disable our bundled extensions in the application directory
+pref("extensions.autoDisableScopes", 11);
+pref("extensions.shownSelectionUI", true);
