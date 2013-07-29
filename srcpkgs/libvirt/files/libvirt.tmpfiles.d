6d7efce3ee4ec805fa6df5b25d74e7ae8fb557f1
Juan RP <xtraeme@gmail.com>
2013-07-29 23:35:32 +0200

    libvirt: update to 1.1.0.

--- srcpkgs/libvirt/files/libvirt.tmpfiles.d
+++ srcpkgs/libvirt/files/libvirt.tmpfiles.d
@@ -1,4 +1,4 @@
-d /var/run/libvirt/qemu 0755 root root -
-d /var/run/libvirt/lxc 0755 root root -
-d /var/run/libvirt/uml 0755 root root -
-d /var/run/libvirt/network 0755 root root -
+d /run/libvirt/qemu 0755 root root -
+d /run/libvirt/lxc 0755 root root -
+d /run/libvirt/uml 0755 root root -
+d /run/libvirt/network 0755 root root -
