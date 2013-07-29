0373ecd789d0e86287ee58cd0863bb26cb79f350
Ypnose <linuxien@legtux.org>
2013-07-29 17:14:21 +0200

    New package: bar-git-20132907

---
+++ srcpkgs/bar-git/files/config.h
@@ -0,0 +1,28 @@
+/* The height of the bar (in pixels) */
+#define BAR_HEIGHT  18
+/* The width of the bar. Set to -1 to fit screen */
+#define BAR_WIDTH   -1
+/* Offset from the left. Set to 0 to have no effect */
+#define BAR_OFFSET 0
+/* Choose between an underline or an overline */
+#define BAR_UNDERLINE 1
+/* The thickness of the underline (in pixels). Set to 0 to disable. */
+#define BAR_UNDERLINE_HEIGHT 2
+/* Default bar position, overwritten by '-b' switch */
+#define BAR_BOTTOM 0
+/* The fonts used for the bar, comma separated. Only the first 2 will be used. */
+#define BAR_FONT       "-*-terminus-medium-r-normal-*-12-*-*-*-c-*-*-1","fixed"
+/* Some fonts don't set the right width for some chars, pheex it */
+#define BAR_FONT_FALLBACK_WIDTH 6
+/* Color palette */
+#define COLOR0  0x1A1A1A /* background */
+#define COLOR1  0xA9A9A9 /* foreground */
+#define COLOR2  0x303030
+#define COLOR3  0xF92672
+#define COLOR4  0xA6E22E
+#define COLOR5  0xFD971F
+#define COLOR6  0x66D9EF
+#define COLOR7  0x9E6FFE
+#define COLOR8  0xAF875F
+#define COLOR9  0xCCCCC6
+
