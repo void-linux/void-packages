6b8b12f123d7e54d015c2858092c0e3b657fc6c1
Juan RP <xtraeme@gmail.com>
2014-01-28 17:50:55 +0100

    environment/0002-cross.sh: only pass cross configure_args in some cases.

--- common/environment/0002-cross.sh
+++ common/environment/0002-cross.sh
@@ -5,4 +5,7 @@
 export PKG_CONFIG_SYSROOT_DIR="$XBPS_CROSS_BASE"
 export PKG_CONFIG_PATH="$XBPS_CROSS_BASE/lib/pkgconfig:$XBPS_CROSS_BASE/usr/share/pkgconfig"
 export PKG_CONFIG_LIBDIR="$XBPS_CROSS_BASE/lib/pkgconfig"
-export configure_args+=" --host=$XBPS_CROSS_TRIPLET --with-sysroot=$XBPS_CROSS_BASE --with-libtool-sysroot=$XBPS_CROSS_BASE "
+
+if [ "$build_style" = "gnu-configure" -o -z "$build_style" ]; then
+	export configure_args+=" --host=$XBPS_CROSS_TRIPLET --with-sysroot=$XBPS_CROSS_BASE --with-libtool-sysroot=$XBPS_CROSS_BASE "
+fi
