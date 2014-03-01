4a716cd999fb1dce137ff99fdd0f0a5611134d87
Juan RP <xtraeme@gmail.com>
2014-03-01 09:50:24 +0100

    New cross profile: armv7hf-musl.

---
+++ common/cross-profiles/armv7hf-musl.sh
@@ -0,0 +1,8 @@
+# Cross build profile for ARMv7 EABI Hard Float and Musl libc.
+
+XBPS_TARGET_ARCH="armv7l-musl"
+XBPS_CROSS_TRIPLET="arm-linux-musleabi"
+XBPS_CFLAGS="-O2 -pipe"
+XBPS_CXXFLAGS="$XBPS_CFLAGS"
+XBPS_CROSS_CFLAGS="-march=armv7-a -mfpu=vfpv3 -mfloat-abi=hard"
+XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
