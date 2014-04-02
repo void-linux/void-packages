7d7e8217540f9e9573fd062d2b49c5f2dd4dd3f5
Juan RP <xtraeme@gmail.com>
2014-04-02 10:16:16 +0200

    hooks/post-install/strip: better regexp for shlib-provides from Gottox.

--- common/hooks/post-install/03-strip-and-debug-pkgs.sh
+++ common/hooks/post-install/03-strip-and-debug-pkgs.sh
@@ -106,9 +106,9 @@ hook() {
 			fi
 			echo "   Stripped library: ${f#$PKGDESTDIR}"
 			_soname=$(${OBJDUMP} -p "$f"|grep SONAME|awk '{print $2}')
-			pattern="^lib[[:alnum:]]*([-(\.*)[[:alnum:]]*)\.(so\.[0-9]*|so)"
-			if [[ $fname =~ $pattern ]] && [[ ${_soname} =~ $pattern ]]; then
-				if [ ! -f ${PKGDESTDIR}/usr/lib/${fname} ]; then
+			pattern="^lib[[:alnum:]_-]+\.so(\.[0-9]+)*$"
+			if [[ ${_soname} =~ $pattern ]]; then
+				if [ ! -e ${PKGDESTDIR}/usr/lib/${fname} ]; then
 					continue
 				fi
 				echo "${_soname}" >> ${PKGDESTDIR}/.shlib-provides
