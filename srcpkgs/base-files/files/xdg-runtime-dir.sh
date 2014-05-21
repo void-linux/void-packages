6c6d397a8300730deaca86867877f1ffeda5edfc
Juan RP <xtraeme@gmail.com>
2014-05-21 08:44:43 +0200

    base-files: new profile script that provides XDG_RUNTIME_DIR if unset.
    This makes weston work without systemd.

---
+++ srcpkgs/base-files/files/xdg-runtime-dir.sh
@@ -0,0 +1,9 @@
+# Sets and creates XDG_RUNTIME_DIR.
+
+if test -z "${XDG_RUNTIME_DIR}"; then
+	export XDG_RUNTIME_DIR=/tmp/${UID}-runtime-dir
+	if ! test -d "${XDG_RUNTIME_DIR}"; then
+		mkdir "${XDG_RUNTIME_DIR}"
+		chmod 0700 "${XDG_RUNTIME_DIR}"
+	fi
+fi
