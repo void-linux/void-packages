ff4663480ecbf1f88b7dd4408f7edbb868869aae
Juan RP <xtraeme@gmail.com>
2014-05-23 09:58:45 +0200

    hooks/script-wrapper: sdl2-config and libpng-config wrappers; enabled in build phase too.

--- common/hooks/pre-configure/02-script-wrapper.sh
+++ common/hooks/pre-configure/02-script-wrapper.sh
@@ -8,10 +8,29 @@ WRAPPERDIR="${wrksrc}/.xbps/bin"
 generic_wrapper() {
 	local wrapper="$1"
 	[ ! -x ${XBPS_CROSS_BASE}/usr/bin/${wrapper} ] && return 0
+	[ -x ${WRAPPERDIR}/${wrapper} ] && return 0
 
 	echo "#!/bin/sh" >> ${WRAPPERDIR}/${wrapper}
 	echo "exec ${XBPS_CROSS_BASE}/usr/bin/${wrapper} --prefix=${XBPS_CROSS_BASE}/usr \"\$@\"" >> ${WRAPPERDIR}/${wrapper}
 	chmod 755 ${WRAPPERDIR}/${wrapper}
+}
+
+libpng_config_wrapper() {
+	[ ! -x ${XBPS_CROSS_BASE}/usr/bin/libpng-config ] && return 0
+	[ -x ${WRAPPERDIR}/libpng-config ] && return 0
+
+	cat >>${WRAPPERDIR}/libpng-config<<_EOF
+#!/bin/sh
+if [ "\$1" = "--prefix" ]; then
+	echo "${XBPS_CROSS_BASE}/usr"
+elif [ "\$1" = "--cflags" ]; then
+	echo "-I${XBPS_CROSS_BASE}/usr/include/libpng16"
+else
+	echo "exec ${XBPS_CROSS_BASE}/usr/bin/libpng-config "\$@"
+fi
+exit \$?
+_EOF
+	chmod 755 ${WRAPPERDIR}/libpng-config
 }
 
 hook() {
@@ -24,6 +43,8 @@ hook() {
 	generic_wrapper libgcrypt-config
 	generic_wrapper freetype-config
 	generic_wrapper sdl-config
+	generic_wrapper sdl2-config
+	libpng_config_wrapper
 
 	export PATH=${WRAPPERDIR}:$PATH
 }
