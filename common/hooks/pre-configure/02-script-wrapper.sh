75bfe1c813dc2a373f0d480deaf02db26d07951b
Juan RP <xtraeme@gmail.com>
2014-06-25 07:36:21 +0200

    hooks/script-wrapper: create cups-config wrapper.

--- common/hooks/pre-configure/02-script-wrapper.sh
+++ common/hooks/pre-configure/02-script-wrapper.sh
@@ -63,6 +63,7 @@ hook() {
 	generic_wrapper sdl2-config
 	generic_wrapper gpgme-config
 	generic_wrapper imlib2-config
+	generic_wrapper cups-config
 	generic_wrapper2 gpg-error-config
 	generic_wrapper2 libpng-config
 	generic_wrapper2 ncurses5-config
