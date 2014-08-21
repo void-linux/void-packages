8fc5850fd4fbc55e413b9659a989b40d1850a460
Juan RP <xtraeme@gmail.com>
2014-08-21 16:56:03 +0200

    cross-profiles/armv7hf: use the arm-linux-gnueabihf cross compiler.
    There's no reason to use the gnueabihf7 variant, because the only difference
    are the CFLAGS to build it. Use the gnueabihf one (used by Rpi too).

--- common/cross-profiles/armv7hf.sh
+++ common/cross-profiles/armv7hf.sh
@@ -1,7 +1,7 @@
 # Cross build profile for ARMv7 GNU EABI Hard Float.
 
 XBPS_TARGET_ARCH="armv7l"
-XBPS_CROSS_TRIPLET="arm-linux-gnueabihf7"
+XBPS_CROSS_TRIPLET="arm-linux-gnueabihf"
 XBPS_CFLAGS="-O2 -pipe -fstack-protector-strong"
 XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CFLAGS="-march=armv7-a -mfpu=vfpv3 -mfloat-abi=hard"
