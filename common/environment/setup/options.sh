6d20b9a05af48b21db51b67d165004c53979ce2f
Juan RP <xtraeme@gmail.com>
2014-08-30 09:10:11 +0200

    xbps-src: move vopt_* helpers to environment/setup/options.sh.

---
+++ common/environment/setup/options.sh
@@ -0,0 +1,22 @@
+# vim: set ts=4 sw=4 et:
+
+vopt_if() {
+    local opt="$1" t="$2" f="$3"
+    name="build_option_$opt"
+    if [ ${!name} ]; then
+        echo -n "$t"
+    else
+        echo -n "$f"
+    fi
+}
+
+vopt_with() {
+    local opt="$1" flag="${2:-$1}"
+    vopt_if "$1" "--with-${flag}" "--without-${flag}"
+}
+
+vopt_enable() {
+    local opt="$1" flag="${2:-$1}"
+    vopt_if "$1" "--enable-${flag}" "--disable-${flag}"
+}
+
