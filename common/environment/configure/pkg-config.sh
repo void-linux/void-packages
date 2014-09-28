f7146ea652fb80a51d6cf5c9e385d060ff30a5f2
Juan RP <xtraeme@gmail.com>
2014-09-28 08:01:18 +0200

    environment/pkg-config: unset PKG_CONFIG; find it via PATH instead.
    ... otherwise some pkgs fail to build due to host vs target (gtk+3).

--- common/environment/configure/pkg-config.sh
+++ common/environment/configure/pkg-config.sh
@@ -3,6 +3,3 @@
 if [ -z "$CHROOT_READY" ]; then
 	export PKG_CONFIG_PATH="${XBPS_MASTERDIR}/usr/lib/pkgconfig:${XBPS_MASTERDIR}/usr/share/pkgconfig"
 fi
-if [ -n "$CROSS_BUILD" ]; then
-	export PKG_CONFIG="${XBPS_CROSS_TRIPLET}-pkg-config"
-fi
