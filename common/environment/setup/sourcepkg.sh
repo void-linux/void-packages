b2976da1c8ea82929a82555b56c3a6c82aadec85
Juan RP <xtraeme@gmail.com>
2014-10-11 09:57:27 +0200

    xbps-src: use "disable_debug" to disable -dbg subpkg creation.

--- common/environment/setup/sourcepkg.sh
+++ common/environment/setup/sourcepkg.sh
@@ -16,7 +16,7 @@ unset -v CC CXX CPP GCC LD AR AS RANLIB NM OBJDUMP OBJCOPY STRIP READELF
 unset -v skip_extraction
 
 # hooks/post-install/03-strip-and-debug-pkgs
-unset -v force_debug_pkgs
+unset -v disable_debug force_debug_pkgs
 
 ## FUNCTIONS
 unset -f pre_fetch do_fetch post_fetch
--- common/hooks/post-install/03-strip-and-debug-pkgs.sh
+++ common/hooks/post-install/03-strip-and-debug-pkgs.sh
@@ -6,7 +6,7 @@
 make_debug() {
 	local dname= fname= dbgfile=
 
-	[ -z "$XBPS_DEBUG_PKGS" -o -n "$nonfree" ] && return 0
+	[ -z "$XBPS_DEBUG_PKGS" -o -n "$disable_debug" -o -n "$nonfree" ] && return 0
 
 	dname=$(echo "$(dirname $1)"|sed -e "s|${PKGDESTDIR}||g")
 	fname="$(basename $1)"
@@ -25,7 +25,7 @@ make_debug() {
 attach_debug() {
 	local dname= fname= dbgfile=
 
-	[ -z "$XBPS_DEBUG_PKGS" -o -n "$nonfree" ] && return 0
+	[ -z "$XBPS_DEBUG_PKGS" -o -n "$disable_debug" -o -n "$nonfree" ] && return 0
 
 	dname=$(echo "$(dirname $1)"|sed -e "s|${PKGDESTDIR}||g")
 	fname="$(basename $1)"
@@ -41,7 +41,7 @@ attach_debug() {
 create_debug_pkg() {
 	local _pkgname= _destdir=
 
-	[ -z "$XBPS_DEBUG_PKGS" -o -n "$nonfree" ] && return 0
+	[ -z "$XBPS_DEBUG_PKGS" -o -n "$disable_debug" -o -n "$nonfree" ] && return 0
 	[ ! -d "${PKGDESTDIR}/usr/lib/debug" ] && return 0
 
 	_pkgname="${pkgname}-dbg-${version}"
