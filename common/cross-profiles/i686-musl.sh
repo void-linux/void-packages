3774e0e0615c1d51115d1bcacdf02a94c585c096
Juan RP <xtraeme@gmail.com>
2015-03-06 10:28:26 +0100

    cross-profile: i686-musl: disable fstack-protector-strong for now.
    crc32.lo: In function `crc32_combine_':
    crc32.c:(.text+0x1d1): undefined reference to `__stack_chk_fail_local'

--- common/cross-profiles/i686-musl.sh
+++ common/cross-profiles/i686-musl.sh
@@ -3,4 +3,6 @@
 XBPS_TARGET_ARCH="i686-musl"
 XBPS_CROSS_TRIPLET="i686-linux-musl"
 XBPS_CROSS_CFLAGS="-march=i686"
+XBPS_CFLAGS="-O2 -pipe -fstack-protector --param ssp-buffer-size=2"
+XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
