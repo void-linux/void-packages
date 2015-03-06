9000a1e907e2f2085327b350863f5bcffb6706fa
Juan RP <xtraeme@gmail.com>
2015-03-06 08:23:46 +0100

    cross-profiles: use working C{,XX}FLAGS for cross pkgs that use gcc-4.7.x.

--- common/cross-profiles/armv5te.sh
+++ common/cross-profiles/armv5te.sh
@@ -3,4 +3,6 @@
 XBPS_TARGET_ARCH="armv5tel"
 XBPS_CROSS_TRIPLET="arm-linux-gnueabi"
 XBPS_CROSS_CFLAGS="-march=armv5te -msoft-float -mfloat-abi=soft"
+XBPS_CFLAGS="-O2 -pipe -fstack-protector --param ssp-buffer-size=2"
+XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
--- common/cross-profiles/mipsel.sh
+++ common/cross-profiles/mipsel.sh
@@ -3,4 +3,6 @@
 XBPS_TARGET_ARCH="mipsel"
 XBPS_CROSS_TRIPLET="mipsel-softfloat-linux-gnu"
 XBPS_CROSS_CFLAGS="-mtune=mips32r2 -mabi=32 -msoft-float"
+XBPS_CFLAGS="-O2 -pipe -fstack-protector --param ssp-buffer-size=2"
+XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
