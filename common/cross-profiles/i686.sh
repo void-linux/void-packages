b095cbfbfee8594d9bdef27fbf25d7c2170e14aa
Juan RP <xtraeme@gmail.com>
2015-03-06 08:20:09 +0100

    cross-profiles: i686: use -fstack-protector for now (close #1061).
    The cross-i686-pc-linux-gnu pkg uses gcc-4.7.x and does not support
    -fstack-protector-strong, so use a more relaxed approach for now.

--- common/cross-profiles/i686.sh
+++ common/cross-profiles/i686.sh
@@ -2,5 +2,7 @@
 
 XBPS_TARGET_ARCH="i686"
 XBPS_CROSS_TRIPLET="i686-pc-linux-gnu"
-XBPS_CROSS_CFLAGS="-march=i686 -mtune=generic"
+XBPS_CFLAGS="-O2 -pipe -fstack-protector --param ssp-buffer-size=2"
+XBPS_CXXFLAGS="$XBPS_CFLAGS"
+XBPS_CROSS_CFLAGS="-march=i686"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
