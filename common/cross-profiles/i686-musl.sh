ee5d8644befe9beaf70e2bc446beb444c7b459e8
Juan RP <xtraeme@gmail.com>
2015-03-06 10:29:40 +0100

    cross-profiles/i686-musl: disable SSP completely.

--- common/cross-profiles/i686-musl.sh
+++ common/cross-profiles/i686-musl.sh
@@ -3,6 +3,6 @@
 XBPS_TARGET_ARCH="i686-musl"
 XBPS_CROSS_TRIPLET="i686-linux-musl"
 XBPS_CROSS_CFLAGS="-march=i686"
-XBPS_CFLAGS="-O2 -pipe -fstack-protector --param ssp-buffer-size=2"
+XBPS_CFLAGS="-O2 -pipe"
 XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
