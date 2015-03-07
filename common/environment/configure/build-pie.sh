ea96fc3ccea0335eac71027250915307b783a3d6
Christian Neukirchen <chneukirchen@gmail.com>
2015-03-07 20:52:28 +0100

    xbps-src: pass PIE flags via -specs mechanism, avoids breaking static build.

--- common/environment/configure/build-pie.sh
+++ common/environment/configure/build-pie.sh
@@ -1,4 +1,6 @@
+_GCCSPECSDIR=${XBPS_COMMONDIR}/environment/configure/gccspecs
+
 if [ -n "$build_pie" ]; then
-	CFLAGS+=" -fPIE"
-	LDFLAGS+=" -pie"
+	CFLAGS+=" -specs=$_GCCSPECSDIR/hardened-cc1"
+	LDFLAGS+=" -specs=$_GCCSPECSDIR/hardened-ld"
 fi
