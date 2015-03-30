64bbc33ca2a7e2d7d44f51e7cb61ea2e40ca922b
Juan RP <xtraeme@gmail.com>
2015-03-30 08:33:00 +0200

    hooks/script-wrapper: create libetpan-config wrapper.

--- common/hooks/pre-configure/02-script-wrapper.sh
+++ common/hooks/pre-configure/02-script-wrapper.sh
@@ -109,6 +109,7 @@ hook() {
 	generic_wrapper3 xapian-config
 	generic_wrapper3 ncurses5-config
 	generic_wrapper3 ncursesw5-config
+	generic_wrapper3 libetpan-config
 	python_wrapper python-config 2.7
 	python_wrapper python3.4-config 3.4m
 
