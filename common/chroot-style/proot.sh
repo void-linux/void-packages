ed6345f93864d66260368fc73ccd96bec9c267b3
Juan RP <xtraeme@gmail.com>
2015-04-09 19:30:28 +0200

    xbps-src: unbreak chroot-style scripts.

--- common/chroot-style/proot.sh
+++ common/chroot-style/proot.sh
@@ -6,19 +6,17 @@ readonly MASTERDIR="$1"
 readonly DISTDIR="$2"
 readonly HOSTDIR="$3"
 readonly EXTRA_ARGS="$4"
-readonly COMMAND="$5"
-shift 5
-readonly COMMAND_ARGS="$@"
+shift 4
 
 if ! command -v proot >/dev/null 2>&1; then
 	exit 1
 fi
 
-if [ -z "$MASTERDIR" -o -z "$DISTDIR" -o -z "$COMMAND" ]; then
-	echo "$0 MASTERDIR/DISTDIR/COMMAND not set"
+if [ -z "$MASTERDIR" -o -z "$DISTDIR" ]; then
+	echo "$0 MASTERDIR/DISTDIR not set"
 	exit 1
 fi
 
 exec proot -r $XBPS_MASTERDIR -w / -b $DISTDIR:/void-packages \
 	${HOSTDIR:+-b $HOSTDIR:/host} -b /proc:/proc -b /dev:/dev \
-	-b /sys:/sys $EXTRA_ARGS $COMMAND $COMMAND_ARGS
+	-b /sys:/sys $EXTRA_ARGS $@
--- common/chroot-style/uunshare.sh
+++ common/chroot-style/uunshare.sh
@@ -6,17 +6,15 @@ readonly MASTERDIR="$1"
 readonly DISTDIR="$2"
 readonly HOSTDIR="$3"
 readonly EXTRA_ARGS="$4"
-readonly COMMAND="$5"
-shift 5
-readonly COMMAND_ARGS="$@"
+shift 4
 
 if ! command -v xbps-uunshare >/dev/null 2>&1; then
 	exit 1
 fi
 
-if [ -z "$MASTERDIR" -o -z "$DISTDIR" -o -z "$COMMAND" ]; then
-	echo "$0 MASTERDIR/DISTDIR/COMMAND not set"
+if [ -z "$MASTERDIR" -o -z "$DISTDIR" ]; then
+	echo "$0 MASTERDIR/DISTDIR not set"
 	exit 1
 fi
 
-exec xbps-uunshare $EXTRA_ARGS -D $DISTDIR ${HOSTDIR:+-H $HOSTDIR} $XBPS_MASTERDIR $COMMAND $COMMAND_ARGS
+exec xbps-uunshare $EXTRA_ARGS -D $DISTDIR ${HOSTDIR:+-H $HOSTDIR} $MASTERDIR $@
