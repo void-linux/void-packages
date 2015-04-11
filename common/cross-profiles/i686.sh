240bdb088afe7c715bfad22eef3c7429c4d5f7a0
Juan RP <xtraeme@gmail.com>
2015-04-11 10:29:52 +0200

    cross-i686-pc-linux-gnu: update toolchain components and unbreak.

--- common/cross-profiles/i686.sh
+++ common/cross-profiles/i686.sh
@@ -2,7 +2,5 @@
 
 XBPS_TARGET_ARCH="i686"
 XBPS_CROSS_TRIPLET="i686-pc-linux-gnu"
-XBPS_CFLAGS="-O2 -pipe -fstack-protector --param ssp-buffer-size=2"
-XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CFLAGS="-march=i686"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
