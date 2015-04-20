3f6ee78aac47ed538a2ad570a311f83d901df57a
Juan RP <xtraeme@voidlinux.eu>
2015-04-20 11:39:39 +0200

    travis: fix show_files for cross targets.

--- common/travis/show_files.sh
+++ common/travis/show_files.sh
@@ -2,10 +2,14 @@
 #
 # show_files.sh
 
+if [ "$1" != "$2" ]; then
+	arch="-a $2"
+fi
+
 for pkg in $(cat /tmp/templates); do
 	for subpkg in $(xsubpkg $pkg); do
 		/bin/echo -e "\x1b[32mFiles of $subpkg:\x1b[0m"
-		./xbps-src -H $HOME/hostdir show-files "$pkg"
+		./xbps-src -H $HOME/hostdir $arch show-files "$pkg"
 	done
 done
 
