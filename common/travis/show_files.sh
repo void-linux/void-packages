5726bb645230b16d067f8fe09cd611cc6338bd18
Juan RP <xtraeme@voidlinux.eu>
2015-04-23 09:46:40 +0200

    travis: fix show_files for subpkgs.

--- common/travis/show_files.sh
+++ common/travis/show_files.sh
@@ -9,7 +9,7 @@ fi
 for pkg in $(cat /tmp/templates); do
 	for subpkg in $(xsubpkg $pkg); do
 		/bin/echo -e "\x1b[32mFiles of $subpkg:\x1b[0m"
-		./xbps-src -H $HOME/hostdir $arch show-files "$pkg"
+		./xbps-src -H $HOME/hostdir $arch show-files "$subpkg"
 	done
 done
 
