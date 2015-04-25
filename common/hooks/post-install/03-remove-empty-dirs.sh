ffefcc41a5ffe9d1077217baaf94ed199834e6b9
jbu <pullmoll@t-online.de>
2015-04-25 13:30:01 +0200

    xbps-src: Don't print rmdir output

--- common/hooks/post-install/03-remove-empty-dirs.sh
+++ common/hooks/post-install/03-remove-empty-dirs.sh
@@ -4,7 +4,7 @@ hook() {
 	find ${PKGDESTDIR} -type d -empty -print0|sort -z -r|while IFS="" read f; do
 		_dir="${f##${PKGDESTDIR}}"
 		[ -z "${_dir}" ] && continue
-		rmdir --ignore-fail-on-non-empty -p "$f" # &>/dev/null
+		rmdir --ignore-fail-on-non-empty -p "$f" &>/dev/null
 		msg_warn "$pkgver: removed empty dir: ${_dir}\n"
 	done
 	# Create PKGDESTDIR in case it has been removed previously.
