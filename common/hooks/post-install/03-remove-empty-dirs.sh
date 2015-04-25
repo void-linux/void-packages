3a665efea1b34b20df50bbc2c130c06ac75e1961
jbu <pullmoll@t-online.de>
2015-04-25 13:26:18 +0200

    xbps-src: Fix read loop for empty dirs; closes #1409

--- common/hooks/post-install/03-remove-empty-dirs.sh
+++ common/hooks/post-install/03-remove-empty-dirs.sh
@@ -1,10 +1,10 @@
 # This hooks removes empty dirs and warns about them.
 
 hook() {
-	for f in $(find ${PKGDESTDIR} -type d -empty|sort -r); do
+	find ${PKGDESTDIR} -type d -empty -print0|sort -z -r|while IFS="" read f; do
 		_dir="${f##${PKGDESTDIR}}"
 		[ -z "${_dir}" ] && continue
-		rmdir --ignore-fail-on-non-empty -p "$f" &>/dev/null
+		rmdir --ignore-fail-on-non-empty -p "$f" # &>/dev/null
 		msg_warn "$pkgver: removed empty dir: ${_dir}\n"
 	done
 	# Create PKGDESTDIR in case it has been removed previously.
