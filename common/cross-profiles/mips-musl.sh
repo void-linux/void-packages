dbf77e4dcd0eff40de83a94ebdee1be12b4cc5c2
Juan RP <xtraeme@voidlinux.eu>
2015-05-02 05:56:12 +0200

    xbps-src: added support to cross compile for MIPS32 (r2, be, sf) with musl.

---
+++ common/cross-profiles/mips-musl.sh
@@ -0,0 +1,6 @@
+# Cross build profile for MIPS32 BE soft float.
+
+XBPS_TARGET_ARCH="mips-musl"
+XBPS_CROSS_TRIPLET="mips-linux-musl"
+XBPS_CROSS_CFLAGS="-mtune=mips32r2 -mabi=32 -msoft-float"
+XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
