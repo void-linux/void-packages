e950a5d0c85fb0aeacb2775703681dfd4e5e60b0
Juan RP <xtraeme@voidlinux.eu>
2015-05-02 12:04:41 +0200

    cross-profiles/aarch64-musl: -fstack-protector-strong works.

--- common/cross-profiles/aarch64-musl.sh
+++ common/cross-profiles/aarch64-musl.sh
@@ -2,7 +2,5 @@
 
 XBPS_TARGET_ARCH="aarch64-musl"
 XBPS_CROSS_TRIPLET="aarch64-linux-musl"
-XBPS_CFLAGS="-O2 -pipe" # XXX not yet supported: -fstack-protector-strong
-XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CFLAGS="-march=armv8-a"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
