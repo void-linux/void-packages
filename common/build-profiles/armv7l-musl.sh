33a2ce7cb0751e3118fe3edd18aaaf1a7f1f83d6
Juan RP <xtraeme@voidlinux.eu>
2015-05-17 08:01:04 +0200

    New package: cross-armv7l-linux-musleabihf for armv7hf-musl.

--- common/build-profiles/armv7l-musl.sh
+++ common/build-profiles/armv7l-musl.sh
@@ -1,3 +1,3 @@
 XBPS_TARGET_CFLAGS="-march=armv7-a -mfpu=vfpv3 -mfloat-abi=hard"
 XBPS_TARGET_CXXFLAGS="$XBPS_CXXFLAGS"
-XBPS_TRIPLET="armv7l-unknown-linux-musleabi"
+XBPS_TRIPLET="armv7l-linux-musleabihf"
--- common/cross-profiles/armv7hf-musl.sh
+++ common/cross-profiles/armv7hf-musl.sh
@@ -1,6 +1,6 @@
 # Cross build profile for ARMv7 EABI Hard Float and Musl libc.
 
 XBPS_TARGET_ARCH="armv7l-musl"
-XBPS_CROSS_TRIPLET="arm-linux-musleabi"
+XBPS_CROSS_TRIPLET="armv7l-linux-musleabihf"
 XBPS_CROSS_CFLAGS="-march=armv7-a -mfpu=vfpv3 -mfloat-abi=hard"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
