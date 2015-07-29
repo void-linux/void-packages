e96a88a027d7458318a761d8fceab51c2c6b278a
Juan RP <xtraeme@voidlinux.eu>
2015-07-29 10:24:53 +0200

    build-style/ruby-module: force LANG=C to workaround musl.

--- common/build-style/ruby-module.sh
+++ common/build-style/ruby-module.sh
@@ -5,5 +5,5 @@
 do_install() {
 	local _vendorlibdir=$(ruby -e 'puts RbConfig::CONFIG["vendorlibdir"]')
 
-	ruby install.rb --destdir=${DESTDIR} --sitelibdir=${_vendorlibdir} ${make_install_args}
+	LANG=C ruby install.rb --destdir=${DESTDIR} --sitelibdir=${_vendorlibdir} ${make_install_args}
 }
