1ecea12d64caca42036ae16519ce8fb3cb7d935d
Andrea Brancaleoni <miwaxe@gmail.com>
2015-08-21 08:58:34 +0200

    ulatencyd: fix subtle + unbreak build

--- srcpkgs/ulatencyd/files/subtle.lua
+++ srcpkgs/ulatencyd/files/subtle.lua
@@ -18,7 +18,7 @@ SubtleWM = {
 	end
 }
 
-AwesomeFix = RunnerFix.new("SubtleWM", {"subtle"})
+SubtleFix = RunnerFix.new("SubtleWM", {"subtle"})
 
 -- on start we have to fix all processes that have descented from kde
 local function cleanup_awesome_mess()
@@ -27,4 +27,4 @@ local function cleanup_awesome_mess()
 end
 
 ulatency.add_timeout(cleanup_awesome_mess, 1000)
-ulatency.register_filter(AwesomeUI)
+ulatency.register_filter(SubtleWM)
