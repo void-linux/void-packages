f362068f7c3c51ffd7a65826fab7c92376bb6c43
Juan RP <xtraeme@voidlinux.eu>
2015-09-05 10:22:41 +0200

    xbps-src: new "scons" build-style.

---
+++ common/build-style/scons.sh
@@ -0,0 +1,20 @@
+#
+# This helper is for templates using Qt4/Qt5 qmake.
+#
+do_build() {
+	: ${make_cmd:=scons}
+
+	${make_cmd} ${makejobs} CC=$CC CXX=$CXX CCFLAGS="$CFLAGS" \
+		CXXFLAGS="$CXXFLAGS" LINKFLAGS="$LDFLAGS" \
+		prefix=/usr DESTDIR=${DESTDIR} \
+		${make_build_args} ${make_build_target}
+}
+do_install() {
+	: ${make_cmd:=scons}
+	: ${make_install_target:=install}
+
+	${make_cmd} ${makejobs} CC=$CC CXX=$CXX CCFLAGS="$CFLAGS" \
+		CXXFLAGS="$CXXFLAGS" LINKFLAGS="$LDFLAGS" \
+		prefix=/usr DESTDIR=${DESTDIR} \
+		${make_install_args} ${make_install_target}
+}
