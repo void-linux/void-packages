5093d6b0c109654b2ed378581e8883634f501d25
Christian Neukirchen <chneukirchen@gmail.com>
2015-09-14 16:13:13 +0200

    New build_style haskell-stack.

---
+++ common/build-style/haskell-stack.sh
@@ -0,0 +1,28 @@
+#
+# This helper is for templates built using Haskell stack.
+#
+# hostmakedepends="ghc stack"
+# make_build_args="stack-build-flags"
+# stackage="lts-X.Y"  # or include a stack.yaml in $FILESDIR
+#
+do_build() {
+	if [ -f "${FILESDIR}/stack.yaml" ]; then
+		msg_normal "Using stack config in stack.yaml.\n"
+		cp "${FILESDIR}/stack.yaml" .
+	else
+		if [ -z "$stackage" ]; then
+			msg_error "Stackage version not set in \$stackage.\n"
+		fi
+		msg_normal "Using stackage resolver ${stackage}.\n"
+		STACK_ROOT=$wrksrc/.stack \
+			stack init --force --resolver ${stackage}
+	fi
+
+	STACK_ROOT=$wrksrc/.stack stack ${makejobs} build ${make_build_args}
+}
+
+do_install() {
+	vmkdir usr/bin
+	STACK_ROOT=$wrksrc/.stack stack install ${make_build_args} \
+		--local-bin-path=${DESTDIR}/usr/bin
+}
