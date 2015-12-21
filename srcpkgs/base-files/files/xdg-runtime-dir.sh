dc0aa047ce2f58fe775fe1624183aac1b7d55c9a
Juan RP <xtraeme@voidlinux.eu>
2015-12-21 16:20:38 +0100

    base-files: check /tmp is writable before creating $XDG_RUNTIME_DIR.

--- srcpkgs/base-files/files/xdg-runtime-dir.sh
+++ srcpkgs/base-files/files/xdg-runtime-dir.sh
@@ -2,8 +2,8 @@
 
 if test -z "${XDG_RUNTIME_DIR}"; then
 	export XDG_RUNTIME_DIR=/tmp/${UID:-$(id -u)}-runtime-dir
-	if ! test -d "${XDG_RUNTIME_DIR}"; then
-		mkdir "${XDG_RUNTIME_DIR}"
-		chmod 0700 "${XDG_RUNTIME_DIR}"
+	if test -w /tmp -a ! test -d "${XDG_RUNTIME_DIR}"; then
+		mkdir -p "${XDG_RUNTIME_DIR}" >/dev/null 2>&1
+		chmod 0700 "${XDG_RUNTIME_DIR}" >/dev/null 2>&1
 	fi
 fi
