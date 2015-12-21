fc23959d0e42765e8d4e83be1b3988d355bcfa66
Juan RP <xtraeme@voidlinux.eu>
2015-12-21 16:26:07 +0100

    base-files: improve previous.

--- srcpkgs/base-files/files/xdg-runtime-dir.sh
+++ srcpkgs/base-files/files/xdg-runtime-dir.sh
@@ -1,8 +1,8 @@
 # Sets and creates XDG_RUNTIME_DIR.
 
-if test -z "${XDG_RUNTIME_DIR}"; then
+if [ -z "${XDG_RUNTIME_DIR}" ]; then
 	export XDG_RUNTIME_DIR=/tmp/${UID:-$(id -u)}-runtime-dir
-	if test -w /tmp -a ! test -d "${XDG_RUNTIME_DIR}"; then
+	if [ -w /tmp ] && [ ! -d "${XDG_RUNTIME_DIR}"; then
 		mkdir -p "${XDG_RUNTIME_DIR}" >/dev/null 2>&1
 		chmod 0700 "${XDG_RUNTIME_DIR}" >/dev/null 2>&1
 	fi
