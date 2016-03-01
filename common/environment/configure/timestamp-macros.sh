c46b43df61872843d548f97638e9ea9000832b83
Enno Boland <g@s01.de>
2016-03-01 17:45:11 +0100

    common/environment: add missing backslashes for macro definitions.

--- common/environment/configure/timestamp-macros.sh
+++ common/environment/configure/timestamp-macros.sh
@@ -2,7 +2,7 @@ if [ -n "$XBPS_COMMIT_TIMESTAMP" ]; then
 	CFLAGS+=" -Wno-builtin-macro-redefined"
 	CXXFLAGS+=" -Wno-builtin-macro-redefined"
 	for i in "DATE,%b\x20%d\x20%Y" "TIME,%H:%M:%S" "DATETIME,%b\x20%d\x20%Y\x20%H:%M:%S"; do
-		CFLAGS+=" -U__${i%%,*}__ -D__${i%%,*}__=\\\"$(LC_ALL=C date --date "$XBPS_COMMIT_TIMESTAMP" +"${i#*,}")\\\""
-		CXXFLAGS+=" -U__${i%%,*}__ -D__${i%%,*}__=\\\"$(LC_ALL=C date --date "$XBPS_COMMIT_TIMESTAMP" +"${i#*,}")\\\""
+		CFLAGS+=" -U__${i%%,*}__ -D__${i%%,*}__=\\\\\\\"$(LC_ALL=C date --date "$XBPS_COMMIT_TIMESTAMP" +"${i#*,}")\\\\\\\""
+		CXXFLAGS+=" -U__${i%%,*}__ -D__${i%%,*}__=\\\\\\\"$(LC_ALL=C date --date "$XBPS_COMMIT_TIMESTAMP" +"${i#*,}")\\\\\\\""
 	done
 fi
