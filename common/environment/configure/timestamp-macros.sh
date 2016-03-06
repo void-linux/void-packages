0c4b812ad3d25118ddd33789210e147764625030
Enno Boland <g@s01.de>
2016-03-06 14:10:41 +0100

    common/environment: fix error message

--- common/environment/configure/timestamp-macros.sh
+++ common/environment/configure/timestamp-macros.sh
@@ -1,7 +1,7 @@
 if [ -n "$SOURCE_DATE_EPOCH" ]; then
 	CFLAGS+=" -Wno-builtin-macro-redefined -include$XBPS_STATEDIR/timestamp-macros.h"
 	CXXFLAGS+=" -Wno-builtin-macro-redefined -include$XBPS_STATEDIR/timestamp-macros.h"
-	printf > $XBPS_STATEDIR/timestamp-macros.h
+	printf "" > $XBPS_STATEDIR/timestamp-macros.h
 	for i in "DATE,%b %d %Y" "TIME,%H:%M:%S" "DATETIME,%b %d %Y %H:%M:%S"; do
 		mcr=${i%%,*}
 		val=$(LC_ALL=C date --date "@$SOURCE_DATE_EPOCH" +"${i#*,}")
