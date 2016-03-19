72cf5d3f1c73a999d805605cf85c9afcd5853f59
Juan RP <xtraeme@voidlinux.eu>
2016-03-19 07:35:17 +0100

    base-files: get rid of /etc/profile.d/xdg-*.
    - xdg-runtime-dir: this is not necessary anymore, XDG_RUNTIME_DIR
    is set by ConsoleKit2.
    - xdg-user-dirs: this should be created elsewhere not inconditionally.
    
    Close #3374

--- srcpkgs/base-files/files/xdg-runtime-dir.sh
+++
@@ -1,9 +0,0 @@
-# Sets and creates XDG_RUNTIME_DIR.
-
-if [ -z "${XDG_RUNTIME_DIR}" ]; then
-	export XDG_RUNTIME_DIR=/tmp/${UID:-$(id -u)}-runtime-dir
-	if [ -w /tmp ] && [ ! -d "${XDG_RUNTIME_DIR}" ]; then
-		mkdir -p "${XDG_RUNTIME_DIR}" >/dev/null 2>&1
-		chmod 0700 "${XDG_RUNTIME_DIR}" >/dev/null 2>&1
-	fi
-fi
