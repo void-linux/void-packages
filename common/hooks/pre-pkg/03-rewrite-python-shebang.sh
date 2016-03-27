6606995831a0e1ef40cd9f68a1d02f38ec4dd524
Enno Boland <gottox@voidlinux.eu>
2016-03-27 12:24:00 +0200

    common/hooks: fix python shebang rewrite for multi-versioned packages

--- common/hooks/pre-pkg/03-rewrite-python-shebang.sh
+++ common/hooks/pre-pkg/03-rewrite-python-shebang.sh
@@ -4,15 +4,23 @@
 hook() {
 	local pyver= shebang= warn=
 
-	for i in $python_versions; do
-		if [ "$pyver" ]; then
-			warn=1
-			break;
-		fi
-		pyver=$i
-	done
+	case $pkgname in
+	python-*)
+		pyver=2.7;;
+	python3.4-*)
+		pyver=3.4;;
+	*)
+		for i in $python_versions; do
+			if [ "$pyver" ]; then
+				warn=1
+				break;
+			fi
+			pyver=$i
+		done
+		: ${pyver:=2.7}
+		;;
+	esac
 
-	: ${pyver:=2.7}
 	shebang="#!/usr/bin/python$pyver"
 	find ${PKGDESTDIR} -type f -print0 | \
 		xargs -0 grep -H -b -m 1 "^#!.*\([[:space:]]\|/\)python\([[:space:]]\|$\)" -- | while IFS=: read -r f off _; do
