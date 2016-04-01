c07e381f94c5b1ab08641fd2e4c2fe918064bb21
Enno Boland <gottox@voidlinux.eu>
2016-04-01 08:08:52 +0200

    hooks/pre-pkg: fix "integer expression expected" error.

--- common/hooks/pre-pkg/03-rewrite-python-shebang.sh
+++ common/hooks/pre-pkg/03-rewrite-python-shebang.sh
@@ -26,7 +26,7 @@ hook() {
 	shebang="#!/usr/bin/python$pyver"
 	find ${PKGDESTDIR} -type f -print0 | \
 		xargs -0 grep -H -b -m 1 "^#!.*\([[:space:]]\|/\)python\([[:space:]]\|$\)" -- | while IFS=: read -r f off _; do
-		[ "$off" -eq 0 ] || continue
+		[ "$off" -a "$off" -eq 0 ] || continue
 		if [ "$warn" ]; then
 			msg_warn "$pkgname: multiple python versions defined!"
 			msg_warn "$pkgname: using $pyver for shebang"
