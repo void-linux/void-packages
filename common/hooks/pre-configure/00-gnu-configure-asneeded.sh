3db4efd013708c55e919ba9b3c17757370e9b4eb
Enno Boland <gottox@voidlinux.eu>
2016-05-16 20:37:00 +0200

    hooks/pre-configure: do not set configure_script outside of build_style.
    fixes #4201.

--- common/hooks/pre-configure/00-gnu-configure-asneeded.sh
+++ common/hooks/pre-configure/00-gnu-configure-asneeded.sh
@@ -1,13 +1,13 @@
 # This hook enables ld(1) --as-needed in gnu-configure packages.
 
 hook() {
-	: ${configure_script:=./configure}
+	local conf_script=${configure_script:-./configure}
 
-	if [ ! -f "${configure_script}" ]; then
+	if [ ! -f "${conf_script}" ]; then
 		return 0
 	fi
 	# http://lists.gnu.org/archive/html/libtool-patches/2004-06/msg00002.html
 	if [ "$build_style" = "gnu-configure" ]; then
-		sed -i "s/^\([ \t]*tmp_sharedflag\)='-shared'/\1='-shared -Wl,--as-needed'/" ${configure_script}
+		sed -i "s/^\([ \t]*tmp_sharedflag\)='-shared'/\1='-shared -Wl,--as-needed'/" ${conf_script}
 	fi
 }
