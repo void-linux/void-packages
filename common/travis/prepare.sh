d2a6beb2e3652a848a0a9b533b816475aca6a192
Michael Gehring <mg@ebfe.org>
2016-11-17 16:54:21 +0000

    travis: switch to xbps-uchroot and newer os

--- common/travis/prepare.sh
+++ common/travis/prepare.sh
@@ -4,14 +4,13 @@
 
 mkdir -p $HOME/bin
 
-/bin/echo -e '\x1b[32mInstalling proot...\x1b[0m'
-wget -q http://static.proot.me/proot-x86_64
-install -m 755 proot-x86_64 $HOME/bin/proot || exit 1
-
 /bin/echo -e '\x1b[32mInstalling xbps...\x1b[0m'
 wget -q -O - http://repo.voidlinux.eu/static/xbps-static-latest.x86_64-musl.tar.xz | \
 	unxz | tar x -C $HOME/bin --wildcards "./usr/bin/xbps-*" \
 	--strip-components=3 || exit 1
+
+sudo chown root $HOME/bin/xbps-uchroot
+sudo chmod u+s $HOME/bin/xbps-uchroot
 
 /bin/echo -e '\x1b[32mInstalling xtools...\x1b[0m'
 wget -q -O - https://github.com/chneukirchen/xtools/archive/master.tar.gz | \
@@ -19,5 +18,5 @@ wget -q -O - https://github.com/chneukirchen/xtools/archive/master.tar.gz | \
 	--strip-components=1 || exit 1
 
 /bin/echo -e '\x1b[32mUpdating etc/conf...\x1b[0m'
-echo XBPS_CHROOT_CMD=proot >> etc/conf
+echo XBPS_CHROOT_CMD=uchroot >> etc/conf
 echo XBPS_MAKEJOBS=4 >> etc/conf
