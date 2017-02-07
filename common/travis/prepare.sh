82435959133502612a49f804bb4a46c6589cc0b4
Toyam Cox <Vaelatern@gmail.com>
2017-02-06 21:48:14 -0500

    travis: build restricted packages
    After all, we are not about to distribute these binaries.

--- common/travis/prepare.sh
+++ common/travis/prepare.sh
@@ -20,3 +20,4 @@ wget -q -O - https://github.com/chneukirchen/xtools/archive/master.tar.gz | \
 /bin/echo -e '\x1b[32mUpdating etc/conf...\x1b[0m'
 echo XBPS_CHROOT_CMD=uchroot >> etc/conf
 echo XBPS_MAKEJOBS=4 >> etc/conf
+echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
