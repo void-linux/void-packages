18cc5fc716eb7799d17bb64614280f358f120378
Lon Willett <xgit@lonw.net>
2017-05-31 07:33:37 +0200

    New package: skype-5.3.0.1
    Repackaging of the Debian version of Skype for Void.
    
    Replaces the skype-32bit package (no longer supported).
    
    Closes: #6558 [via git-merge-pr]

--- srcpkgs/skype/files/skype.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-MACHINE=$(uname -m)
-if [ "$MACHINE" = "x86_64" ]; then
-	export LD_LIBRARY_PATH=/usr/lib32
-	export QT_PLUGIN_PATH=/usr/lib32/qt/plugins
-fi
-
-PULSE_LATENCY_MSEC=30 exec /usr/lib32/skype/skype "$@"
