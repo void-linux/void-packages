e4fca68c323311573af40eee0b762830abb26abc
Helmut Pozimski <helmut@pozimski.eu>
2017-08-23 09:04:15 +0200

    kodi: update to 17.4

--- srcpkgs/kodi/files/swig.nojava-17.4/AddonModuleXbmc.i.cpp
+++ srcpkgs/kodi/files/swig.nojava-17.4/AddonModuleXbmc.i.cpp
@@ -9218,7 +9218,7 @@ namespace PythonBindings
 
    // constants
    PyModule_AddStringConstant(module, (char*)"__author__", (char*)"Team Kodi <http://kodi.tv>");
-   PyModule_AddStringConstant(module, (char*)"__date__", (char*)"Fri May 26 08:01:11 GMT 2017");
+   PyModule_AddStringConstant(module, (char*)"__date__", (char*)"Wed Aug 23 05:23:28 GMT 2017");
    PyModule_AddStringConstant(module, (char*)"__version__", (char*)"2.25.0");
    PyModule_AddStringConstant(module, (char*)"__credits__", (char*)"Team Kodi");
    PyModule_AddStringConstant(module, (char*)"__platform__", (char*)"ALL");
