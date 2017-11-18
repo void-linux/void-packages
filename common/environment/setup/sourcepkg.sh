a8b8efe636d95ca434351ca25f86b9741a27d535
Michael Gehring <mg@ebfe.org>
2017-11-18 12:14:25 +0000

    common/environment/setup/sourcepkg.sh: unset {pre,do,post}_check, checkdepends

--- common/environment/setup/sourcepkg.sh
+++ common/environment/setup/sourcepkg.sh
@@ -7,7 +7,7 @@ unset -v only_for_archs distfiles checksum build_style nocross broken
 unset -v configure_script configure_args wrksrc build_wrksrc create_wrksrc
 unset -v make_cmd make_build_args make_install_args make_build_target make_install_target python_version stackage
 unset -v patch_args disable_parallel_build keep_libtool_archives make_use_env
-unset -v reverts subpackages makedepends hostmakedepends depends restricted
+unset -v reverts subpackages makedepends hostmakedepends checkdepends depends restricted
 unset -v nopie build_options build_options_default bootstrap repository reverts
 unset -v CFLAGS CXXFLAGS FFLAGS CPPFLAGS LDFLAGS LD_LIBRARY_PATH
 unset -v CC CXX CPP GCC LD AR AS RANLIB NM OBJDUMP OBJCOPY STRIP READELF
@@ -23,5 +23,6 @@ unset -f pre_fetch do_fetch post_fetch
 unset -f pre_extract do_extract post_extract
 unset -f pre_configure do_configure post_configure
 unset -f pre_build do_build post_build
+unset -f pre_check do_check post_check
 unset -f pre_install do_install post_install
 unset -f do_clean
