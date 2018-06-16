38cfc63064f4d7dda0371f26643b3ffa71516812
Nicolas Porcel <nicolasporcel06@gmail.com>
2018-06-16 20:36:25 +0200

    New package: runit-iptables-20180618

---
+++ srcpkgs/runit-iptables/files/91-iptables.sh
@@ -0,0 +1,9 @@
+if [ -e /etc/iptables/iptables.rules ]
+then
+  iptables-restore /etc/iptables/iptables.rules
+fi
+
+if [ -e /etc/iptables/ip6tables.rules ]
+then
+  ip6tables-restore /etc/iptables/ip6tables.rules
+fi
