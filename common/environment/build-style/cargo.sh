0318d0385acc2dc790087a81421982fc2de21357
Rasmus Thomsen <rasmus.thomsen@protonmail.com>
2018-09-16 21:39:23 +0200

    build-style/cargo.sh: add

---
+++ common/environment/build-style/cargo.sh
@@ -0,0 +1,10 @@
+hostmakedepends+=" cargo"
+
+if [ "$CROSS_BUILD" ]; then
+	makedepends+=" rust"
+fi
+
+# Use the config we set in do_configure
+export CARGO_HOME="$wrksrc/.cargo"
+
+export PKG_CONFIG_ALLOW_CROSS=1
