5f5a55c2bb7456599bc5fe03f1cc613681265069
Johannes <johannes.brechtmann@gmail.com>
2018-11-19 20:19:24 +0100

    build-style/cargo.sh: export common variables to avoid static linking

--- common/environment/build-style/cargo.sh
+++ common/environment/build-style/cargo.sh
@@ -5,3 +5,9 @@ if [ "$CROSS_BUILD" ]; then
 fi
 
 export PKG_CONFIG_ALLOW_CROSS=1
+
+export LIBGIT2_SYS_USE_PKG_CONFIG=1
+export GETTEXT_BIN_DIR=/usr/bin
+export GETTEXT_LIB_DIR="${XBPS_CROSS_BASE}/usr/lib/gettext"
+export GETTEXT_INCLUDE_DIR="${XBPS_CROSS_BASE}/usr/include"
+export LIBSSH2_SYS_USE_PKG_CONFIG=1
