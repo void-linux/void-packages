4bd5f1e0953d2dc7023d1043be15b8d7e9f81397
Rasmus Thomsen <rasmus.thomsen@protonmail.com>
2018-11-29 17:14:41 +0100

    chromium: enable VAAPI by default & fix on i686
    The '--enable-gpu-rasterization' configure parameter is
    required to enable VAAPI (hardware accelerated video
    decoding). This commit adds this parameter to the
    chromium wrapper script
    
    [ci skip]

--- srcpkgs/chromium/files/chromium.sh
+++ srcpkgs/chromium/files/chromium.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 export CHROME_WRAPPER=/usr/lib/chromium/chromium
 export CHROME_DESKTOP=chromium.desktop
+CHROME_FLAGS="--enable-gpu-rasterization"
 exec /usr/lib/chromium/chromium $CHROME_FLAGS "$@"
