904f74edf21a672193c19cd5f66414a283437535
maxice8 <thinkabit.ukim@gmail.com>
2019-01-20 19:47:57 -0200

    environment/build-style/cargo.sh: remove variables meant for common.sh

--- common/environment/build-style/cargo.sh
+++ common/environment/build-style/cargo.sh
@@ -3,11 +3,3 @@ hostmakedepends+=" cargo"
 if [ "$CROSS_BUILD" ]; then
 	makedepends+=" rust-std"
 fi
-
-export PKG_CONFIG_ALLOW_CROSS=1
-
-export LIBGIT2_SYS_USE_PKG_CONFIG=1
-export GETTEXT_BIN_DIR=/usr/bin
-export GETTEXT_LIB_DIR="${XBPS_CROSS_BASE}/usr/lib/gettext"
-export GETTEXT_INCLUDE_DIR="${XBPS_CROSS_BASE}/usr/include"
-export LIBSSH2_SYS_USE_PKG_CONFIG=1
