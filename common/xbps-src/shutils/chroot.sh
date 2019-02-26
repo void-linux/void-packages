64e66745b6c609ed680ca7a9de2ad900b1805d9d
maxice8 <thinkabit.ukim@gmail.com>
2019-02-26 17:23:11 -0300

    common: add chroot-style 'ethereal'

--- common/xbps-src/shutils/chroot.sh
+++ common/xbps-src/shutils/chroot.sh
@@ -216,6 +216,7 @@ chroot_handler() {
         env -i -- PATH="/usr/bin:/usr/sbin:$PATH" SHELL=/bin/sh \
             HOME=/tmp IN_CHROOT=1 LC_COLLATE=C LANG=en_US.UTF-8 \
             SOURCE_DATE_EPOCH="$SOURCE_DATE_EPOCH" \
+            XBPS_ALLOW_CHROOT_BREAKOUT="$XBPS_ALLOW_CHROOT_BREAKOUT" \
             $XBPS_COMMONDIR/chroot-style/${XBPS_CHROOT_CMD:=uunshare}.sh \
             $XBPS_MASTERDIR $XBPS_DISTDIR "$XBPS_HOSTDIR" "$XBPS_CHROOT_CMD_ARGS" \
             /void-packages/xbps-src $action $pkg
