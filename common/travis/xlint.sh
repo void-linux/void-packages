ca6659d7fb6f8979021e071be5ef3c9891e32437
maxice8 <thinkabit.ukim@gmail.com>
2019-03-05 22:47:54 -0300

    travis: use ethereal.

--- common/travis/xlint.sh
+++ common/travis/xlint.sh
@@ -4,4 +4,7 @@
 
 [ "$XLINT" ] || exit 0 
 
-awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates | xargs xlint
+awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates | while read -r t; do
+	/bin/echo -e "\x1b[32mLinting $t...\x1b[0m"
+	xlint "$t"
+done
