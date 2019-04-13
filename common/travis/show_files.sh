e1b3b5cf487a5fdbd67d457b65fe3c07a167e651
maxice8 <thinkabit.ukim@gmail.com>
2019-04-13 13:49:41 -0300

    travis/show_files.sh: avoid useless use of cat

--- common/travis/show_files.sh
+++ common/travis/show_files.sh
@@ -4,10 +4,9 @@
 
 export XBPS_TARGET_ARCH="$2" XBPS_DISTDIR=/hostrepo
 
-for pkg in $(cat /tmp/templates); do
+while read -r pkg; do
 	for subpkg in $(xsubpkg $pkg); do
 		/bin/echo -e "\x1b[32mFiles of $subpkg:\x1b[0m"
 		xbps-query --repository=$HOME/hostdir/binpkgs -f "$subpkg"
 	done
-done
-
+done < /tmp/templates
