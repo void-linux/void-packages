50bffa5ad0fad6a6a95e7157f380ade2cd8905e9
Juan RP <xtraeme@gmail.com>
2019-04-13 13:43:07 +0200

    xbps-src: reduce basename(1) usage.
    This saves a few exec()s.

--- common/build-style/fetch.sh
+++ common/build-style/fetch.sh
@@ -5,7 +5,8 @@ do_extract() {
 
 	mkdir -p ${wrksrc}
 	for f in ${distfiles}; do
-		curfile=$(basename "${f#*>}")
+		curfile="${f#*>}"
+		curfile="${curfile##*/}"
 		cp ${XBPS_SRCDISTDIR}/${pkgname}-${version}/${curfile} ${wrksrc}/${curfile}
 	done
 }
