17a905aee8d2bf6b96e612f7cc3bd95df949f3aa
Jony <--get>
2019-07-15 10:21:17 -0700

    common/environment/setup/misc.sh: sorted list of sites and added GITHUB_SITE

--- common/environment/setup/misc.sh
+++ common/environment/setup/misc.sh
@@ -4,19 +4,20 @@
 # SITE used for ditfiles mirrors. For use in $distfiles.
 set -a
 
-SOURCEFORGE_SITE="http://downloads.sourceforge.net/sourceforge"
+CPAN_SITE="https://www.cpan.org/modules/by-module"
+DEBIAN_SITE="http://ftp.debian.org/debian/pool"
+FREEDESKTOP_SITE="https://freedesktop.org/software"
+GITHUB_SITE="https://github.com"
+GNOME_SITE="http://ftp.gnome.org/pub/GNOME/sources"
+GNU_SITE="http://ftp.gnu.org/gnu"
+KDE_SITE="https://download.kde.org/stable"
+KERNEL_SITE="https://www.kernel.org/pub/linux"
+MOZILLA_SITE="https://ftp.mozilla.org/pub"
 NONGNU_SITE="http://download.savannah.nongnu.org/releases"
+PYPI_SITE="https://files.pythonhosted.org/packages/source"
+SOURCEFORGE_SITE="http://downloads.sourceforge.net/sourceforge"
 UBUNTU_SITE="http://archive.ubuntu.com/ubuntu/pool"
 XORG_SITE="https://www.x.org/releases/individual"
-DEBIAN_SITE="http://ftp.debian.org/debian/pool"
-GNOME_SITE="http://ftp.gnome.org/pub/GNOME/sources"
-KERNEL_SITE="https://www.kernel.org/pub/linux"
-CPAN_SITE="https://www.cpan.org/modules/by-module"
-PYPI_SITE="https://files.pythonhosted.org/packages/source"
-MOZILLA_SITE="https://ftp.mozilla.org/pub"
-GNU_SITE="http://ftp.gnu.org/gnu"
-FREEDESKTOP_SITE="https://freedesktop.org/software"
-KDE_SITE="https://download.kde.org/stable"
 
 # Repetitive sub homepage's with no real project page
 # ie. some gnome and xorg projects. For use in $homepage.
