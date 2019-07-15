ed3635f24f3be4ce6c9205f259550b0a5e3bfd1a
Piotr Wójcik <chocimier@tlen.pl>
2019-07-15 20:49:45 +0200

    travis: lint license

--- common/travis/xlint.sh
+++ common/travis/xlint.sh
@@ -2,7 +2,10 @@
 #
 # xlint.sh
 
-[ "$XLINT" ] || exit 0 
+[ "$XLINT" ] || exit 0
+
+mkdir -p /usr/share/spdx/
+cp common/travis/license.lst /usr/share/spdx/
 
 awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates | while read -r t; do
 	/bin/echo -e "\x1b[32mLinting $t...\x1b[0m"
