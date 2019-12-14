23ff92670036ca8fd96dfadb3f40475205ed8420
Cameron Nemo <cnemo@tutanota.com>
2019-12-14 15:40:22 -0800

    base-files: install /usr/lib32 symlink on i686

--- common/hooks/pre-install/00-lib32.sh
+++ common/hooks/pre-install/00-lib32.sh
@@ -1,7 +1,8 @@
-# This hook creates the /usr/lib32 symlink for x86.
+# This hook creates the /usr/lib32 symlink for 32-bit systems.
 
 hook() {
-	if [ "$XBPS_TARGET_MACHINE" = "i686" ]; then
+	if [ "$XBPS_TARGET_WORDSIZE" = "32" ] && \
+	   [ "${pkgname}" != "base-files" ]; then
 		vmkdir usr/lib
 		ln -sf lib ${PKGDESTDIR}/usr/lib32
 	fi
