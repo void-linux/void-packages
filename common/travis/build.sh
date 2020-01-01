08cbbf1764554f2a800634ad627535e1b5608ab4
Juan RP <xtraeme@gmail.com>
2020-01-01 11:57:45 +0100

    travis: some tweaks for base-chroot-v2:
    - no need to perform `boostrap-update` anymore, xbps-src does it now.
    - don't set useless values to `etc/conf`.

--- common/travis/build.sh
+++ common/travis/build.sh
@@ -6,27 +6,6 @@ if [ "$1" != "$2" ]; then
 	arch="-a $2"
 fi
 
-# Tell xbps-src what is our arch, this is done when doing
-# binary-bootstrap, but we need to do it every time since
-# our masterdir is ethereal.
-# /bin/echo -e '\x1b[32mWriting bootstrap arch into .xbps_chroot_init of masterdir\x1b[0m'
-# echo "$1" > /hostrepo/masterdir/.xbps_chroot_init
-
-/bin/echo -e '\x1b[32mPreparing chroot with chroot_prepare()\x1b[0m'
-source hostrepo/common/xbps-src/shutils/chroot.sh || {
-	echo "Failed to source chroot.sh for chroot_prepare()" >&2 ;
-	exit 1
-}
-
-XBPS_SRCPKGDIR=/hostrepo/srcpkgs XBPS_MASTERDIR=/ chroot_prepare $1 || {
-	echo "Failed to prepare chroot!" >&2 ;
-	exit 1
-}
-
-# Two times due to updating xbps itself
-/hostrepo/xbps-src -H "$HOME"/hostdir bootstrap-update
-/hostrepo/xbps-src -H "$HOME"/hostdir bootstrap-update
-
 PKGS=$(/hostrepo/xbps-src sort-dependencies $(cat /tmp/templates))
 
 NPROCS=1
--- common/travis/prepare.sh
+++ common/travis/prepare.sh
@@ -5,8 +5,6 @@
 [ "$XLINT" ] && exit 0
 
 /bin/echo -e '\x1b[32mUpdating etc/conf...\x1b[0m'
-echo XBPS_CHROOT_CMD=uchroot >> etc/conf
-echo XBPS_MAKEJOBS=4 >> etc/conf
 echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
 
 /bin/echo -e '\x1b[32mEnabling ethereal chroot-style...\x1b[0m'
