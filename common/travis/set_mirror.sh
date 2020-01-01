e4a5beb11634f094adbef06934d029a25d4f2a32
Juan RP <xtraeme@gmail.com>
2020-01-01 11:45:45 +0100

    travis: update set_mirror.sh for `etc/xbps.d`.

--- common/travis/set_mirror.sh
+++ common/travis/set_mirror.sh
@@ -3,7 +3,7 @@
 TRAVIS_PROTO=http
 TRAVIS_MIRROR=alpha.us.repo.voidlinux.org
 
-for _i in etc/repos-remote.conf etc/defaults.conf etc/repos-remote-x86_64.conf ; do
+for _i in etc/xbps.d/repos-remote*.conf ; do
     /bin/echo -e "\x1b[32mUpdating $_i...\x1b[0m"
     # First fix the proto, ideally we'd serve everything with HTTPS,
     # but key management and rotation is a pain, and things are signed
