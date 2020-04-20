0b0d555edf7841ecce7a69a372958db5755e5399
Đoàn Trần Công Danh <congdanhqx+sgn@gmail.com>
2020-04-20 23:18:14 +0700

    setup/git.sh: prefer git plumbing command
    git-ls-files(1) is plumbing command, its output will never change
    regardless of configuration, version.
    
    git-status(1) output will be changed depends on configuration.
    At least, 986d4dbc7d (common/environment/setup/git.sh: ensure untracked
    files are checked., 2017-11-24) was added to address a different output
    on `status.showUntrackedFiles`.
    
    By doing this, also reduce a pipe, and a fork-exec.
    
    Technically, git-log(1) is also a porcelain, but I _think_ `%ct` is
    stable enough to stay there. If the day has come, that `git-log(1)` can
    be replaced with:
    
        git cat-file commit HEAD |
        sed -ne '/^committer/{s/.* \([0-9]*\) [-+][0-9][0-9][0-9][0-9]$/\1/p;q}'

--- common/environment/setup/git.sh
+++ common/environment/setup/git.sh
@@ -9,7 +9,7 @@ if [ -n "$XBPS_USE_BUILD_MTIME" ]; then
 	unset SOURCE_DATE_EPOCH
 elif [ -z "${SOURCE_DATE_EPOCH}" -a -n "$IN_CHROOT" ]; then
 	# check if the template is under version control:
-	if $XBPS_GIT_CMD -C ${XBPS_SRCPKGDIR}/${basepkg} status -u normal --porcelain template | grep "^?? " &> /dev/null; then
+	if [ -z "$($XBPS_GIT_CMD -C ${XBPS_SRCPKGDIR}/${basepkg} ls-files template)" ]; then
 		export SOURCE_DATE_EPOCH="$(stat -c %Y ${XBPS_SRCPKGDIR}/${basepkg}/template)"
 	else
 		export SOURCE_DATE_EPOCH="$($XBPS_GIT_CMD -C ${XBPS_DISTDIR} log --pretty='%ct' -n1 HEAD)"
