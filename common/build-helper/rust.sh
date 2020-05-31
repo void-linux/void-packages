46ab04b8d1f9c27475f3012d8f2ec2c963a3f63d
Érico Rolim <erico.erc@gmail.com>
2020-05-31 00:02:37 -0300

    common/build-helper/rust.sh: force native SSL lib
    The OPENSSL_NO_VENDOR environment variable forces the openssl-sys crate
    to use the system's SSL library instead of building its own.

--- common/build-helper/rust.sh
+++ common/build-helper/rust.sh
@@ -35,3 +35,6 @@ export LIBSSH2_SYS_USE_PKG_CONFIG=1
 # sodium-sys
 export SODIUM_LIB_DIR="${XBPS_CROSS_BASE}/usr/include"
 export SODIUM_INC_DIR="${XBPS_CROSS_BASE}/usr/lib"
+
+# openssl-sys
+export OPENSSL_NO_VENDOR=1
