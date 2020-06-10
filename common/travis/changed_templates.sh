decd445900111014d502c33fea58f2c6f95b78ac
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-06-10 22:54:08 +0700

    travis/changed_templates: correct left tree-ish
    From b881f32687 (travis/changed_templates: filter by git itself,
    2020-05-23), we replaced git-diff(1) with git-diff-tree(1).
    
    The change wasn't equivalence, though.
    We used to compare between merge-base of FETCH_HEAD and HEAD.
    From that commit, we started to compare FETCH_HEAD and HEAD instead.
    
    Fix them by changing left tree-ish to the merge-base.

--- common/travis/changed_templates.sh
+++ common/travis/changed_templates.sh
@@ -9,7 +9,9 @@ elif command -v git >/dev/null 2>&1; then
 fi
 
 /bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
-$GIT_CMD diff-tree -r --no-renames --name-only --diff-filter=AM FETCH_HEAD HEAD -- 'srcpkgs/*/template' |
+$GIT_CMD diff-tree -r --no-renames --name-only --diff-filter=AM \
+	"$(git merge-base FETCH_HEAD HEAD)" HEAD \
+	-- 'srcpkgs/*/template' |
 	cut -d/ -f 2 |
 	tee /tmp/templates |
 	sed "s/^/  /" >&2
