c21063efe8da15b6c4c2b0629cbf6d37a20a382d
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-06-21 18:46:43 +0700

    build-style/cmake: s/Release/RelWithDebInfo/
    Release uses "-O3" which will overwrite our "-O2".

--- common/build-style/cmake.sh
+++ common/build-style/cmake.sh
@@ -43,7 +43,7 @@ _EOF
 		cmake_args+=" -DCMAKE_TOOLCHAIN_FILE=cross_${XBPS_CROSS_TRIPLET}.cmake"
 	fi
 	cmake_args+=" -DCMAKE_INSTALL_PREFIX=/usr"
-	cmake_args+=" -DCMAKE_BUILD_TYPE=Release"
+	cmake_args+=" -DCMAKE_BUILD_TYPE=RelWithDebInfo"
 
 	if [ "$XBPS_TARGET_MACHINE" = "i686" ]; then
 		cmake_args+=" -DCMAKE_INSTALL_LIBDIR=lib32"
