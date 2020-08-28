111814b9a60da5065bf9f263cb60602f328cc2e4
Piotr Wójcik <chocimier@tlen.pl>
2020-08-28 19:19:14 +0200

    xbps-src: separate options of target package and its dependencies

--- common/xbps-src/shutils/common.sh
+++ common/xbps-src/shutils/common.sh
@@ -192,7 +192,7 @@ set_build_options() {
     done
 
     # Prepare final options.
-    for f in ${!options[@]}; do
+    for f in ${build_options}; do
         if [[ ${options[$f]} -eq 1 ]]; then
             eval export build_option_${f}=1
         else
