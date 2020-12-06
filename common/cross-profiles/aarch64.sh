095bf4734c4665761ac2aa51bcdbf8761c91fd8e
q66 <daniel@octaforge.org>
2020-12-06 06:00:35 +0100

    cross-profiles/aarch64: remove nonsense that does not belong
    ????

--- common/cross-profiles/aarch64.sh
+++ common/cross-profiles/aarch64.sh
@@ -3,8 +3,6 @@
 XBPS_TARGET_MACHINE="aarch64"
 XBPS_TARGET_QEMU_MACHINE="aarch64"
 XBPS_CROSS_TRIPLET="aarch64-linux-gnu"
-XBPS_CFLAGS="-O2 -pipe" # XXX not yet supported: -fstack-protector-strong
-XBPS_CXXFLAGS="$XBPS_CFLAGS"
 XBPS_CROSS_CFLAGS="-march=armv8-a"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
 XBPS_CROSS_FFLAGS=""
