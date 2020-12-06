29255b0244034fb16377aadc193a2bfab6712172
q66 <daniel@octaforge.org>
2020-12-06 05:37:25 +0100

    build-profiles: drop unnecessary cflags for ppc
    we don't need to specify -mcpu where compiler default is used,
    preserve mtune though; do not -mno-altivec on ppc since altivec
    is not default in the first place and doing this will just
    break random things that explicitly enable altivec for runtime
    checked code; do not use -maltivec on either ppc64le or ppc64
    since it's implied by their -mcpu's (power8 for ppc64le which
    is implicit, and 970 for ppc64 which is explicit)
    
    also remove -mlong-double-64 for musl targets as that's compiler
    default and it's not necessary to pass it; also remove -mabi=elfv2
    for both ppc64le and ppc64 since it's implied default

--- common/build-profiles/ppc64le-musl.sh
+++ common/build-profiles/ppc64le-musl.sh
@@ -1,4 +1,4 @@
-XBPS_TARGET_CFLAGS="-mcpu=powerpc64le -mtune=power9 -maltivec -mlong-double-64 -mabi=elfv2"
+XBPS_TARGET_CFLAGS="-mtune=power9"
 XBPS_TARGET_CXXFLAGS="$XBPS_TARGET_CFLAGS"
 XBPS_TARGET_FFLAGS=""
 XBPS_TRIPLET="powerpc64le-unknown-linux-musl"
