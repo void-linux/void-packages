1e86562c728e04077b14d6b272d682b36c4a873f
q66 <daniel@octaforge.org>
2020-12-06 05:41:17 +0100

    cross-profiles: do the same as for build-profiles for ppc

--- common/cross-profiles/ppc64-musl.sh
+++ common/cross-profiles/ppc64-musl.sh
@@ -3,7 +3,7 @@
 XBPS_TARGET_MACHINE="ppc64-musl"
 XBPS_TARGET_QEMU_MACHINE="ppc64"
 XBPS_CROSS_TRIPLET="powerpc64-linux-musl"
-XBPS_CROSS_CFLAGS="-mcpu=970 -mtune=power9 -maltivec -mlong-double-64 -mabi=elfv2"
+XBPS_CROSS_CFLAGS="-mcpu=970 -mtune=power9"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
 XBPS_CROSS_FFLAGS=""
 XBPS_CROSS_RUSTFLAGS="--sysroot=${XBPS_CROSS_BASE}/usr"
