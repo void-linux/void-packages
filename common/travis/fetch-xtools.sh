06f4a9805dcb5cd74dcf848a3952e1152fc01985
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-12-14 08:35:54 +0700

    GitHub Action: fetch xtools instead of installing from repo
    Avoid its dependencies inside build container.

--- common/travis/fetch-xtools.sh
+++ common/travis/fetch-xtools.sh
@@ -5,6 +5,26 @@
 mkdir -p /tmp/bin
 
 /bin/echo -e '\x1b[32mInstalling xtools...\x1b[0m'
-wget -q -O - https://github.com/leahneukirchen/xtools/archive/master.tar.gz | \
-	gunzip | tar x -C /tmp/bin --wildcards "xtools-master/x*" \
-	--strip-components=1 || exit 1
+
+if command -v bsdtar >/dev/null; then
+	TAR=bsdtar
+	_tar_wildcard=
+elif command -v tar >/dev/null; then
+	TAR=tar
+	_tar_wildcard=--wildcards
+else
+	echo "tar and bsdtar: not found"
+	exit 1
+fi
+
+_link=https://github.com/leahneukirchen/xtools/archive/master.tar.gz
+if command -v xbps-fetch >/dev/null; then
+	xbps-fetch $_link >/dev/null 2>&1
+	cat ${_link##*/}
+else
+	wget -q -O - $_link
+fi |
+gunzip |
+$TAR xf - -C /tmp/bin --strip-components=1 ${_tar_wildcard} "xtools-master/x*" ||
+exit 1
+rm -f ${_link##*/}
