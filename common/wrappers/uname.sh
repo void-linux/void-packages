1e1e7079f86475ed1a916bc454cf011de9014219
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2021-01-25 08:56:55 +0700

    common: fix exit status of uname wrapper

--- common/wrappers/uname.sh
+++ common/wrappers/uname.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 uname=$(/usr/bin/uname $@)
+rv=$?
 uname_m=$(/usr/bin/uname -m)
 arch=${XBPS_ARCH%-musl}
 # if XBPS_ARCH was reseted by `env -i` use original `/usr/bin/uname -m`
 : ${arch:=$uname_m}
-rv=$?
 echo "$uname" |
 	sed "s/\(^\| \)$(/usr/bin/uname -n)\($\| \)/\1void\2/" |
 	sed "s/$uname_m/$arch/"
