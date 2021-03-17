60984d4a640f6213b52840e299b708be30e133fb
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2021-03-17 08:11:31 +0700

    build-style/cmake: do not require ninja if make_cmd is make

--- common/environment/build-style/cmake.sh
+++ common/environment/build-style/cmake.sh
@@ -1,5 +1,8 @@
 if [ "$CHROOT_READY" ]; then
 	if [[ "$hostmakedepends" != *"cmake-bootstrap"* ]]; then
-		hostmakedepends+=" cmake ninja"
+		hostmakedepends+=" cmake"
+	fi
+	if [ "${make_cmd:-ninja}" = ninja ]; then
+		hostmakedepends+=" ninja"
 	fi
 fi
