0c9e6e56e79718f89bc6b6efc586bdbd29870328
Michael Aldridge <maldridge@VoidLinux.org>
2021-04-15 01:33:58 -0700

    common/xbps-src: Fix remove-obsoletes

--- common/xbps-src/shutils/remove_obsoletes.sh
+++ common/xbps-src/shutils/remove_obsoletes.sh
@@ -1,6 +1,6 @@
 remove_obsoletes () {
     for repo in $XBPS_HOSTDIR/binpkgs $XBPS_HOSTDIR/binpkgs/debug $XBPS_HOSTDIR/binpkgs/nonfree $XBPS_HOSTDIR/binpkgs/multilib/  $XBPS_HOSTDIR/binpkgs/multilib/nonfree ; do
         msg_normal "Cleaning $repo\n"
-        XBPS_ARCH=${XBPS_CROSS_TARGET:-$XBPS_MACHINE} $XBPS_RINDEX_CMD -r $repo
+        XBPS_ARCH=${XBPS_CROSS_BUILD:-$XBPS_MACHINE} $XBPS_RINDEX_CMD -r $repo
     done
 }
