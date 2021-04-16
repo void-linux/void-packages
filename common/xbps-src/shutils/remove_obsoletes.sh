4f896929b9249ccf1c736fb2deb94a65a78ccb9b
Michael Aldridge <aldridge.mac@gmail.com>
2021-04-16 00:02:06 -0700

    xbps-src: remove remove-obsoletes

--- common/xbps-src/shutils/remove_obsoletes.sh
+++
@@ -1,6 +0,0 @@
-remove_obsoletes () {
-    for repo in $XBPS_HOSTDIR/binpkgs $XBPS_HOSTDIR/binpkgs/debug $XBPS_HOSTDIR/binpkgs/nonfree $XBPS_HOSTDIR/binpkgs/multilib/  $XBPS_HOSTDIR/binpkgs/multilib/nonfree ; do
-        msg_normal "Cleaning $repo\n"
-        XBPS_ARCH=${XBPS_CROSS_BUILD:-$XBPS_MACHINE} $XBPS_RINDEX_CMD -r $repo
-    done
-}
