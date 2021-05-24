55a435486267f7c94bf1d53552fca6938b195b3e
Piotr Wójcik <chocimier@tlen.pl>
2021-05-24 22:13:35 +0200

    common/changed_templates.sh: store merge-base in file

--- common/travis/changed_templates.sh
+++ common/travis/changed_templates.sh
@@ -8,9 +8,11 @@ elif command -v git >/dev/null 2>&1; then
 	GIT_CMD=$(command -v git)
 fi
 
+printf '%s ' "$(git merge-base FETCH_HEAD HEAD)" HEAD  > /tmp/revisions
+
 /bin/echo -e '\x1b[32mChanged packages:\x1b[0m'
 $GIT_CMD diff-tree -r --no-renames --name-only --diff-filter=AM \
-	"$(git merge-base FETCH_HEAD HEAD)" HEAD \
+	$(cat /tmp/revisions) \
 	-- 'srcpkgs/*/template' |
 	cut -d/ -f 2 |
 	tee /tmp/templates |
