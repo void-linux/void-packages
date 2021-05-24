c0331b57eb23ff8f16d82156dee2d78732af5383
Piotr Wójcik <chocimier@tlen.pl>
2021-05-24 22:18:54 +0200

    common: add lint-version-change

--- common/travis/xlint.sh
+++ common/travis/xlint.sh
@@ -5,8 +5,10 @@
 [ "$XLINT" ] || exit 0 
 
 EXITCODE=0
+read base tip < /tmp/revisions
 for t in $(awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates); do
 	/bin/echo -e "\x1b[32mLinting $t...\x1b[0m"
 	xlint "$t" || EXITCODE=$?
+	common/scripts/lint-version-change "$t" $base $tip || EXITCODE=$?
 done
 exit $EXITCODE
