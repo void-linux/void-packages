853f362b05682aee6d15a0ecfc126b280958d473
Michael Aldridge <maldridge@VoidLinux.org>
2021-10-03 17:14:53 -0500

    common/travis: Update mirror URL

--- common/travis/set_mirror.sh
+++ common/travis/set_mirror.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 TRAVIS_PROTO=http
-TRAVIS_MIRROR=alpha.us.repo.voidlinux.org
+TRAVIS_MIRROR=repo-us.voidlinux.org
 
 for _i in etc/xbps.d/repos-remote*.conf ; do
     /bin/echo -e "\x1b[32mUpdating $_i...\x1b[0m"
