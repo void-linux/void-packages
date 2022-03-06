3ebc806005013563ba7a790df69d9f2db93df266
Leah Neukirchen <leah@vuxu.org>
2022-03-06 02:00:16 +0100

    build-style/void-cross.sh: adjustments for --enable-autolink-libatomic.

--- common/build-style/void-cross.sh
+++ common/build-style/void-cross.sh
@@ -134,7 +134,7 @@ _void_cross_build_bootstrap_gcc() {
 		--disable-libmudflap \
 		--disable-libssp \
 		--disable-libitm \
-		--disable-libatomic \
+		--disable-libatomic --disable-autolink-libatomic \
 		--disable-gcov \
 		--disable-threads \
 		--disable-sjlj-exceptions \
@@ -376,6 +376,10 @@ _void_cross_build_gcc() {
 	local ver=$2
 
 	msg_normal "Building gcc for ${tgt}\n"
+
+	# GIANT HACK: create an empty libatomic.a so gcc cross-compile
+	# below works.
+	ar r ${wrksrc}/build_root/usr/${tgt}/usr/lib/libatomic.a
 
 	mkdir -p ${wrksrc}/gcc_build
 	cd ${wrksrc}/gcc_build
