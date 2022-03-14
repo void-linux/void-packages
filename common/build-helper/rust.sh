1490e17b585a1e08776f6c17499090c81dd0cc74
classabbyamp <dev@kb6.ee>
2022-03-13 20:51:03 -0400

    common/build-helper/rust.sh: link to libsodium dynamically
    without this envvar, libsodium-sys will attempt to link to libsodium
    staticly, and may fail.

--- common/build-helper/rust.sh
+++ common/build-helper/rust.sh
@@ -44,6 +44,7 @@ export LIBSSH2_SYS_USE_PKG_CONFIG=1
 # sodium-sys
 export SODIUM_LIB_DIR="${XBPS_CROSS_BASE}/usr/include"
 export SODIUM_INC_DIR="${XBPS_CROSS_BASE}/usr/lib"
+export SODIUM_SHARED=1
 
 # openssl-sys
 export OPENSSL_NO_VENDOR=1
