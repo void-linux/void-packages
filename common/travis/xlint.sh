cc27e8e6f7e9c9c02024d6777896c2ec800b7205
0x5c <dev@0x5c.io>
2022-05-02 00:58:34 -0400

    common/travis/xlint.sh: print Github annotations
    Turns xlint, version lint, and commit lint messages into warning or error
    annotaions. All are printed with special highlighting in the action log and
    summary. Xlint and version lint annotations are shown inline in the changes tab
    and commit view. Annotations without line number are shown at line 1.

--- common/travis/xlint.sh
+++ common/travis/xlint.sh
@@ -11,7 +11,8 @@ common/scripts/lint-commits $base $tip || EXITCODE=$?
 
 for t in $(awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates); do
 	/bin/echo -e "\x1b[32mLinting $t...\x1b[0m"
-	xlint "$t" || EXITCODE=$?
-	common/scripts/lint-version-change "$t" $base $tip || EXITCODE=$?
+	xlint "$t" > /tmp/xlint_out || EXITCODE=$?
+	common/scripts/lint-version-change "$t" $base $tip > /tmp/vlint_out || EXITCODE=$?
+	awk -f common/scripts/lint2annotations.awk /tmp/xlint_out /tmp/vlint_out
 done
 exit $EXITCODE
