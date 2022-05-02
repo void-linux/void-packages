e8f08c50443d9c168060587131279dd1629547b7
0x5c <dev@0x5c.io>
2022-05-02 00:58:34 -0400

    testtest

--- common/travis/xlint.sh
+++ common/travis/xlint.sh
@@ -11,7 +11,19 @@ common/scripts/lint-commits $base $tip || EXITCODE=$?
 
 for t in $(awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates); do
 	/bin/echo -e "\x1b[32mLinting $t...\x1b[0m"
-	xlint "$t" || EXITCODE=$?
+	xlint "$t" > /tmp/lint_out || EXITCODE=$?
+#	if [ "$?" -ne 0 ]; then
+		awk '{
+			split($0, a, ": ")
+			split(a[1], b, ":")
+			msg = substr($0, index($0, ": ") + 2)
+			if (b[2]) {
+				line = ",line=" b[2]
+			}
+			print $0
+			printf "::error title=xlint,file=%s%s::%s\n", b[1], line, msg
+		}' /tmp/lint_out
+#	fi
 	common/scripts/lint-version-change "$t" $base $tip || EXITCODE=$?
 done
 exit $EXITCODE
