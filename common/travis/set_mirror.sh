2233ca2edf8c029522e89be5e13801f225c8072f
Michael Aldridge <maldridge@voidlinux.org>
2022-05-27 18:02:22 -0500

    commont/travis: Fix CI mirror

--- common/travis/set_mirror.sh
+++ common/travis/set_mirror.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+TRAVIS_PROTO=http
 TRAVIS_MIRROR=repo-us.voidlinux.org
 
 for _i in etc/xbps.d/repos-remote*.conf ; do
     /bin/echo -e "\x1b[32mUpdating $_i...\x1b[0m"
-    sed -i "s:repo\.voidlinux\.org:$TRAVIS_MIRROR:g" $_i
+    # First fix the proto, ideally we'd serve everything with HTTPS,
+    # but key management and rotation is a pain, and things are signed
+    # so we can afford to be a little lazy at times.
+    sed -i "s:https:$TRAVIS_PROTO:g" $_i
+
+    # Now set the mirror
+    sed -i "s:alpha\.de\.repo\.voidlinux\.org:$TRAVIS_MIRROR:g" $_i
+    sed -i "s:repo-default\.voidlinux\.org:$TRAVIS_MIRROR:g" $_i
 done
