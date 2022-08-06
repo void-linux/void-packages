500a3c8f491e77cabfb9e5db0a00706eb0600ad4
Duncaen <duncaen@voidlinux.org>
2022-08-06 04:54:05 +0200

    xbps-src: clean up fetch hook and allow mirroring

--- common/environment/build-style/R-cran.sh
+++ common/environment/build-style/R-cran.sh
@@ -4,5 +4,6 @@ wrksrc="${XBPS_BUILDDIR}/${pkgname#R-cran-}"
 
 # default to cran
 if [ -z "$distfiles" ]; then
-	distfiles="https://cran.r-project.org/src/contrib/${pkgname#R-cran-}_${version//r/-}.tar.gz"
+	distfiles="https://cran.r-project.org/src/contrib/${pkgname#R-cran-}_${version//r/-}.tar.gz
+	 https://cran.r-project.org/src/contrib/Archive/${pkgname#R-cran-}/${pkgname#R-cran-}_${version//r/-}.tar.gz"
 fi
