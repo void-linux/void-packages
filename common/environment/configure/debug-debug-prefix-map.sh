e670e34a9a3576e71923d93985e0218ef2af4dfe
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2022-08-24 08:24:03 +0700

    C{,XX}FLAGS: change -fdebug-prefix-map for cmake and meson
    Both of them build inside a builddir. This change ease the debugging
    process.

--- common/environment/configure/debug-debug-prefix-map.sh
+++ common/environment/configure/debug-debug-prefix-map.sh
@@ -1,2 +1,13 @@
-CFLAGS="${CFLAGS} -fdebug-prefix-map=$wrksrc=."
-CXXFLAGS="${CXXFLAGS} -fdebug-prefix-map=$wrksrc=."
+case "$build_style" in
+cmake)
+	CFLAGS="${CFLAGS} -fdebug-prefix-map=$wrksrc/${cmake_builddir:-build}=."
+	CXXFLAGS="${CXXFLAGS} -fdebug-prefix-map=$wrksrc/${cmake_builddir:-build}=."
+	;;
+meson)
+	CFLAGS="${CFLAGS} -fdebug-prefix-map=$wrksrc/${meson_builddir:-build}=."
+	CXXFLAGS="${CXXFLAGS} -fdebug-prefix-map=$wrksrc/${meson_builddir:-build}=."
+	;;
+*)
+	CFLAGS="${CFLAGS} -fdebug-prefix-map=$wrksrc=."
+	CXXFLAGS="${CXXFLAGS} -fdebug-prefix-map=$wrksrc=."
+esac
