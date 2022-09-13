30bd211fbd924eb1f913a8ce4d7f973c9c57ae4b
oreo639 <oreo6391@gmail.com>
2022-09-13 10:28:22 -0700

    base-files: don't overwrite existing locale and define default LANG

--- srcpkgs/base-files/files/locale.sh
+++ srcpkgs/base-files/files/locale.sh
@@ -1,8 +1,14 @@
 # Sets up locale system settings from /etc/locale.conf.
 #
-if [ -s /etc/locale.conf ]; then
-	. /etc/locale.conf
+
+if [ -z "$LANG" ]; then
+	if [ -s /etc/locale.conf ]; then
+		. /etc/locale.conf
+	fi
 fi
+
+# define default LANG to C if not already defined
+LANG=${LANG:-C}
 
 export LANG LANGUAGE LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY
 export LC_MESSAGES LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT
