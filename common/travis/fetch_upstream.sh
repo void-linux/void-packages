6f327d73888ac8fe202071b7ff020e826fd90994
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2022-09-16 08:50:39 +0700

    common: simplify logic to find git
    From chroot-git 2.33.1, we always have an executable named git
    for other build infrastructure and lints.
    
    Let's remove the shenanegan to find which git to be used, prepend the
    path to chroot-git's git into $PATH, and let's the shell call the
    correct git for us instead.

--- common/travis/fetch_upstream.sh
+++ common/travis/fetch_upstream.sh
@@ -2,14 +2,10 @@
 #
 # changed_templates.sh
 
-if command -v chroot-git >/dev/null 2>&1; then
-	GIT_CMD=$(command -v chroot-git)
-elif command -v git >/dev/null 2>&1; then
-	GIT_CMD=$(command -v git)
-fi
+PATH="/usr/libexec/chroot-git:$PATH"
 
 # required by git 2.35.2+
-$GIT_CMD config --global --add safe.directory "$PWD"
+git config --global --add safe.directory "$PWD"
 
 /bin/echo -e '\x1b[32mFetching upstream...\x1b[0m'
-$GIT_CMD fetch --depth 200 https://github.com/void-linux/void-packages.git master
+git fetch --depth 200 https://github.com/void-linux/void-packages.git master
