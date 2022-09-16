a29bdaadca0553cceb4b72eb37c5d60d62621021
classabbyamp <void@placeviolette.net>
2022-09-16 01:02:32 -0400

    common/travis/fetch_upstream.sh: remove manual git config safe.directory
    no longer necessary with actions/checkout@v3

--- common/travis/fetch_upstream.sh
+++ common/travis/fetch_upstream.sh
@@ -2,9 +2,6 @@
 #
 # changed_templates.sh
 
-# required by git 2.35.2+
-git config --global --add safe.directory "$PWD"
-
 /bin/echo -e '\x1b[32mFetching upstream...\x1b[0m'
 echo $(pwd)
 git fetch --depth 200 https://github.com/void-linux/void-packages.git master
