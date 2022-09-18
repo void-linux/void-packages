024b391d631d6f7df8f707b7ec3ccbf344aa647a
Andrew J. Hesford <ajh@sideband.org>
2022-09-18 13:34:27 -0400

    python3: update to 3.11.0.
    Also include bluez headers at build time to allow Bluetooth sockets in
    the Python stdlib.
    
    h/t: @sgn

--- common/environment/setup/python.sh
+++ common/environment/setup/python.sh
@@ -7,7 +7,7 @@ py2_lib="usr/lib/python${py2_ver}"
 py2_sitelib="${py2_lib}/site-packages"
 py2_inc="usr/include/python${py2_ver}"
 
-py3_ver="3.10"
+py3_ver="3.11"
 py3_abiver=""
 py3_lib="usr/lib/python${py3_ver}"
 py3_sitelib="${py3_lib}/site-packages"
