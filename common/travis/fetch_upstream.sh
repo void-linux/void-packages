cf2e7397c4f05e257a8a5dba0918144eb3700964
classabbyamp <void@placeviolette.net>
2022-12-16 04:59:55 -0500

    common/travis/fetch_upstream.sh: remove now-unnecessary script
    no longer needed now that the checkout script fetches the entire
    commit history of master and the PR branch

--- common/travis/fetch_upstream.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# changed_templates.sh
-
-# required by git 2.35.2+
-git config --global --add safe.directory "$PWD"
-
-/bin/echo -e '\x1b[32mFetching upstream...\x1b[0m'
-git fetch --depth 200 https://github.com/void-linux/void-packages.git master
