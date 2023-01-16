5de81f076a743431ae2acafa81d0f1f15760d103
John <me@johnnynator.dev>
2023-01-17 00:10:22 +0100

    go: add riscv64

--- common/environment/build-style/go.sh
+++ common/environment/build-style/go.sh
@@ -1,7 +1,7 @@
 if [ -z "$hostmakedepends" -o "${hostmakedepends##*gcc-go-tools*}" ]; then
 	# gc compiler
 	if [ -z "$archs" ]; then
-		archs="aarch64* armv[567]* i686* x86_64* ppc64le*"
+		archs="aarch64* armv[567]* i686* x86_64* ppc64le* riscv64*"
 	fi
 	hostmakedepends+=" go"
 	nopie=yes
@@ -9,7 +9,7 @@ else
 	# gccgo compiler
 	if [ -z "$archs" ]; then
 		# we have support for these in our gcc
-		archs="aarch64* armv[567]* i686* x86_64* ppc64*"
+		archs="aarch64* armv[567]* i686* x86_64* ppc64* riscv64*"
 	fi
 	if [ "$CROSS_BUILD" ]; then
 		# target compiler to use; otherwise it'll just call gccgo
@@ -29,6 +29,7 @@ case "$XBPS_TARGET_MACHINE" in
 	ppc*) export GOARCH=ppc;;
 	mipsel*) export GOARCH=mipsle;;
 	mips*) export GOARCH=mips;;
+	riscv64*) export GOARCH=riscv64;;
 esac
 
 export GOPATH="${wrksrc}/_build-${pkgname}-xbps"
