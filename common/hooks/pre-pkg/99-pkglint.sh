d3428929b185ff87c3ecfac0c6957e32815e261d
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2023-02-26 23:28:12 +0700

    pre-pkg/99-pkglint: factor out check_shlib function

--- common/hooks/pre-pkg/99-pkglint.sh
+++ common/hooks/pre-pkg/99-pkglint.sh
@@ -1,8 +1,63 @@
 # This hook checks for common issues related to void.
 
+pkg_lint_check_shlib() {
+	local rev libname _shlib _pkgname _pattern
+	local filename="$1"
+	local mapshlibs=$XBPS_COMMONDIR/shlibs
+
+	rev=${filename#*.so.}
+	libname=${filename%.so*}
+	_shlib=$(echo "$libname"|sed -E 's|\+|\\+|g')
+	_pkgname=$(echo "$pkgname"|sed -E 's|\+|\\+|g')
+	if [ "$rev" = "$filename" ]; then
+		_pattern="^${_shlib}\.so[[:blank:]]+${_pkgname}-[^-]+_[0-9]+"
+	else
+		_pattern="^${_shlib}\.so\.[0-9]+(.*)[[:blank:]]+${_pkgname}-[^-]+_[0-9]+"
+	fi
+	grep -E "${_pattern}" $mapshlibs | { \
+		while read conflictFile conflictPkg ignore; do
+			found=1
+			conflictRev=${conflictFile#*.so.}
+			if [ -n "$ignore" -a "$ignore" != "$XBPS_TARGET_MACHINE" ]; then
+				continue
+			elif [ "$rev" = "$conflictRev" ]; then
+				continue
+			elif [[ ${rev}.* =~ $conflictRev ]]; then
+				continue
+			fi
+			msg_red "${pkgver}: SONAME bump detected: ${libname}.so.${conflictRev} -> ${libname}.so.${rev}\n"
+			msg_red "${pkgver}: please update common/shlibs with this line: \"${libname}.so.${rev} ${pkgver}\"\n"
+			msg_red "${pkgver}: all reverse dependencies should also be revbumped to be rebuilt against ${libname}.so.${rev}:\n"
+			_revdeps=$($XBPS_QUERY_XCMD -Rs ${libname}.so -p shlib-requires|cut -d ' ' -f1)
+			for x in ${_revdeps}; do
+				msg_red "   ${x%:}\n"
+			done
+			msg_error "${pkgver}: cannot continue with installation!\n"
+		done
+		# Try to match provided shlibs in virtual packages.
+		for f in ${provides}; do
+			_vpkgname="$($XBPS_UHELPER_CMD getpkgname ${f} 2>/dev/null)"
+			_spkgname="$(grep "^${filename}" $mapshlibs | cut -d ' ' -f2)"
+			_libpkgname="$($XBPS_UHELPER_CMD getpkgname ${_spkgname} 2>/dev/null)"
+			if [ -z "${_spkgname}" -o  -z "${_libpkgname}" ]; then
+				continue
+			fi
+			if [ "${_vpkgname}" = "${_libpkgname}" ]; then
+				found=1
+				break
+			fi
+		done;
+		if [ -z "$found" ]; then
+			_myshlib="${libname}.so"
+			[ "${_myshlib}" != "${rev}" ] && _myshlib+=".${rev}"
+			msg_normal "${pkgver}: ${_myshlib} not found in common/shlibs.\n"
+		fi;
+	}
+}
+
 hook() {
 	local error=0 filename= rev= libname= conflictPkg= conflictFile=
-	local conflictRev= ignore= found= mapshlibs=$XBPS_COMMONDIR/shlibs
+	local conflictRev= ignore= found=
 	local emptypkg=yes
 
 	set +E
@@ -161,53 +216,6 @@ hook() {
 	fi
 
 	for filename in $(<${PKGDESTDIR}/shlib-provides); do
-		rev=${filename#*.so.}
-		libname=${filename%.so*}
-		_shlib=$(echo "$libname"|sed -E 's|\+|\\+|g')
-		_pkgname=$(echo "$pkgname"|sed -E 's|\+|\\+|g')
-		if [ "$rev" = "$filename" ]; then
-			_pattern="^${_shlib}\.so[[:blank:]]+${_pkgname}-[^-]+_[0-9]+"
-		else
-			_pattern="^${_shlib}\.so\.[0-9]+(.*)[[:blank:]]+${_pkgname}-[^-]+_[0-9]+"
-		fi
-		grep -E "${_pattern}" $mapshlibs | { \
-			while read conflictFile conflictPkg ignore; do
-				found=1
-				conflictRev=${conflictFile#*.so.}
-				if [ -n "$ignore" -a "$ignore" != "$XBPS_TARGET_MACHINE" ]; then
-					continue
-				elif [ "$rev" = "$conflictRev" ]; then
-					continue
-				elif [[ ${rev}.* =~ $conflictRev ]]; then
-					continue
-				fi
-				msg_red "${pkgver}: SONAME bump detected: ${libname}.so.${conflictRev} -> ${libname}.so.${rev}\n"
-				msg_red "${pkgver}: please update common/shlibs with this line: \"${libname}.so.${rev} ${pkgver}\"\n"
-				msg_red "${pkgver}: all reverse dependencies should also be revbumped to be rebuilt against ${libname}.so.${rev}:\n"
-				_revdeps=$($XBPS_QUERY_XCMD -Rs ${libname}.so -p shlib-requires|cut -d ' ' -f1)
-				for x in ${_revdeps}; do
-					msg_red "   ${x%:}\n"
-				done
-				msg_error "${pkgver}: cannot continue with installation!\n"
-			done
-			# Try to match provided shlibs in virtual packages.
-			for f in ${provides}; do
-				_vpkgname="$($XBPS_UHELPER_CMD getpkgname ${f} 2>/dev/null)"
-				_spkgname="$(grep "^${filename}" $mapshlibs | cut -d ' ' -f2)"
-				_libpkgname="$($XBPS_UHELPER_CMD getpkgname ${_spkgname} 2>/dev/null)"
-				if [ -z "${_spkgname}" -o  -z "${_libpkgname}" ]; then
-					continue
-				fi
-				if [ "${_vpkgname}" = "${_libpkgname}" ]; then
-					found=1
-					break
-				fi
-			done;
-			if [ -z "$found" ]; then
-				_myshlib="${libname}.so"
-				[ "${_myshlib}" != "${rev}" ] && _myshlib+=".${rev}"
-				msg_normal "${pkgver}: ${_myshlib} not found in common/shlibs.\n"
-			fi;
-		}
+		pkg_lint_check_shlib "$filename"
 	done
 }
