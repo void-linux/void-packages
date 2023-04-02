4ac0467e4aca8e47de09ecac8fc1fde114c6bfc3
Marcin Puc <tranzystorek.io@protonmail.com>
2023-04-02 10:31:13 +0200

    common/build-style/cargo.sh: switch to sparse index protocol

--- common/build-style/cargo.sh
+++ common/build-style/cargo.sh
@@ -5,13 +5,13 @@
 do_build() {
 	: ${make_cmd:=cargo auditable}
 
-	${make_cmd} build --release --target ${RUST_TARGET} ${configure_args}
+	${make_cmd} --config 'registries.crates-io.protocol="sparse"' build --release --target ${RUST_TARGET} ${configure_args}
 }
 
 do_check() {
 	: ${make_cmd:=cargo auditable}
 
-	${make_check_pre} ${make_cmd} test --release --target ${RUST_TARGET} ${configure_args} \
+	${make_check_pre} ${make_cmd} --config 'registries.crates-io.protocol="sparse"' test --release --target ${RUST_TARGET} ${configure_args} \
 		${make_check_args}
 }
 
@@ -19,7 +19,7 @@ do_install() {
 	: ${make_cmd:=cargo auditable}
 	: ${make_install_args:=--path .}
 
-	${make_cmd} install --target ${RUST_TARGET} --root="${DESTDIR}/usr" \
+	${make_cmd} --config 'registries.crates-io.protocol="sparse"' install --target ${RUST_TARGET} --root="${DESTDIR}/usr" \
 		--offline --locked ${configure_args} ${make_install_args}
 
 	rm -f "${DESTDIR}"/usr/.crates.toml
