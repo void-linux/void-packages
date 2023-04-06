5e0f42673ebb3b390a4baf621b8ea20cd5a2199d
John <me@johnnynator.dev>
2023-04-06 21:29:27 +0200

    build-style/zig-build: workaround bug in binutils

--- common/environment/build-style/zig-build.sh
+++ common/environment/build-style/zig-build.sh
@@ -1,1 +1,10 @@
 hostmakedepends+=" zig"
+
+# GNU Binutils fails on programs compiled/linked (?) with llvm
+# See: https://sourceware.org/bugzilla//show_bug.cgi?id=30237
+#
+if [ "${XBPS_TARGET_MACHINE/-musl/}" = "riscv64" ]; then
+	hostmakedepends+=" llvm"
+	OBJCOPY="llvm-objcopy"
+	STRIP="llvm-strip"
+fi
