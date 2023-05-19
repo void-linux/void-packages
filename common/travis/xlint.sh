049cb76df34dbbd8f8af9ea5fc165427b8b4ac63
classabbyamp <void@placeviolette.net>
2023-05-19 18:58:24 -0400

    temp

--- common/travis/xlint.sh
+++ common/travis/xlint.sh
@@ -9,9 +9,9 @@ common/scripts/lint-commits $base $tip || EXITCODE=$?
 
 for t in $(awk '{ print "srcpkgs/" $0 "/template" }' /tmp/templates); do
 	/bin/echo -e "\x1b[32mLinting $t...\x1b[0m"
-	xlint "$t" > /tmp/xlint_out || EXITCODE=$?
+	xlint "$t" || EXITCODE=$? #> /tmp/xlint_out || EXITCODE=$?
 	xshellcheck "$t" || EXITCODE=$?
 	common/scripts/lint-version-change "$t" $base $tip > /tmp/vlint_out || EXITCODE=$?
-	awk -f common/scripts/lint2annotations.awk /tmp/xlint_out /tmp/vlint_out
+	# awk -f common/scripts/lint2annotations.awk /tmp/xlint_out /tmp/vlint_out
 done
 exit $EXITCODE
