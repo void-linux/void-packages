7be0492ad0109836b0f5ccdb76db3b622ea41001
Gonzalo Tornaría <tornaria@cmat.edu.uy>
2023-12-23 19:40:53 -0300

    ./xbps-src show-build-deps: include checkdepends when using -Q or -K
    Due to this change, `./xbps-src sort-dependencies` will take
    checkdepends into account when using -Q or -K.
    
    Before this commit, if `pkgA` checkdepends on `pkgB`, sort-dependencies
    could still print `pkgA` before `pkgB`. This causes CI to build `pkgB`
    twice: first when building `pkgA`, which forces implicit build of pkgB;
    second when building `pkgB` (explicit, so it will ignore the package is
    already built).
    
    The implementation uses `skip_check_step()` from previous commit, for
    consistency, so checkdepends are only taken into account if the check
    step would be enabled.
    
    In particular, nothing is changed unless -Q or -K flag is passed.
    
    EXAMPLE:
    
    Before:
    ```
    $ ./xbps-src -Q sort-dependencies python3-process-tests python3-pytest-cov
    python3-pytest-cov
    python3-process-tests
    ```
    
    After:
    ```
    $ ./xbps-src -Q sort-dependencies python3-process-tests python3-pytest-cov
    python3-process-tests
    python3-pytest-cov
    ```

--- common/xbps-src/shutils/show.sh
+++ common/xbps-src/shutils/show.sh
@@ -117,7 +117,9 @@ show_pkg_build_depends() {
 }
 
 show_pkg_build_deps() {
-    show_pkg_build_depends "${makedepends} $(setup_pkg_depends '' 1 1)" "${hostmakedepends}"
+    local build_depends="${makedepends} $(setup_pkg_depends '' 1 1)"
+    skip_check_step || build_depends+=" ${checkdepends}"
+    show_pkg_build_depends "${build_depends}" "${hostmakedepends}"
 }
 
 show_pkg_hostmakedepends() {
@@ -126,6 +128,10 @@ show_pkg_hostmakedepends() {
 
 show_pkg_makedepends() {
     show_pkg_build_depends "${makedepends}" ""
+}
+
+show_pkg_checkdepends() {
+    show_pkg_build_depends "${checkdepends}" ""
 }
 
 show_pkg_build_options() {
