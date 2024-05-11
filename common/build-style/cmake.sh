f10887f68302fdb600b7b0f7cc99847763f449d8
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2024-05-11 21:32:19 +0700

    build-style/cmake: common flags for KDE

--- common/build-style/cmake.sh
+++ common/build-style/cmake.sh
@@ -63,6 +63,7 @@ _EOF
 		# QT_HOST_PATH isn't enough in my system,
 		# which have binfmts support on and off
 		cmake_args+=" -DQT_HOST_PATH_CMAKE_DIR=/usr/lib/cmake"
+		cmake_args+=" -DKDE_INSTALL_LIBDIR=lib"
 	fi
 
 	if [[ $build_helper = *"qemu"* ]]; then
