7ffbd5ed6c960e8079beaee9c8d5be570a74e462
classabbyamp <void@placeviolette.net>
2024-08-12 20:04:22 -0400

    *: qemu-user-static -> qemu-user

--- common/build-helper/qemu.sh
+++ common/build-helper/qemu.sh
@@ -1,7 +1,7 @@
 if [ "$CROSS_BUILD" ]; then
 	export QEMU_LD_PREFIX=${XBPS_CROSS_BASE}
-	if [[ $hostmakedepends != *"qemu-user-static"* ]]; then
-		hostmakedepends+=" qemu-user-static"
+	if [[ $hostmakedepends != *"qemu-user"* ]]; then
+		hostmakedepends+=" qemu-user"
 	fi
 fi
 
