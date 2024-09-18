48d3a3b065db0496ea7e3903ab97e8a80b513193
classabbyamp <void@placeviolette.net>
2024-09-18 00:04:30 -0400

    common/build-helper/qemu: only install the relevant user emulator
    now that the package is split, we don't have to install every emulator
    under the sun.

--- common/build-helper/qemu.sh
+++ common/build-helper/qemu.sh
@@ -1,8 +1,6 @@
 if [ "$CROSS_BUILD" ]; then
 	export QEMU_LD_PREFIX=${XBPS_CROSS_BASE}
-	if [[ $hostmakedepends != *"qemu-user"* ]]; then
-		hostmakedepends+=" qemu-user"
-	fi
+	hostmakedepends+=" qemu-user-${XBPS_TARGET_QEMU_MACHINE/x86_64/amd64}"
 fi
 
 vtargetrun() {
