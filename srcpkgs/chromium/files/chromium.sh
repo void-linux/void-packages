0c1017dd633049e863ad9f902b7db447b62be8a5
John <me@johnnynator.dev>
2024-09-19 19:32:17 +0200

    chromium: update to 129.0.6668.58.

--- srcpkgs/chromium/files/chromium.sh
+++ srcpkgs/chromium/files/chromium.sh
@@ -2,4 +2,7 @@
 export CHROME_WRAPPER=/usr/lib/chromium/chromium
 export CHROME_DESKTOP=chromium.desktop
 CHROME_FLAGS="--enable-gpu-rasterization $CHROME_FLAGS"
-exec /usr/lib/chromium/chromium $CHROME_FLAGS "$@"
+case $(xbps-uhelper arch) in
+	*-musl) exec /usr/lib/chromium/chromium $CHROME_FLAGS --js-flags="--jitless --wasm_jitless" "$@";;
+	*) exec /usr/lib/chromium/chromium $CHROME_FLAGS "$@";;
+esac
