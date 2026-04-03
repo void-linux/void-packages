5073c0b470420cd3f94e63dae19a0b2493d1b158
Sebastian Jug <seb@stianj.ug>
2026-04-03 14:54:16 -0400

    fixes

--- srcpkgs/rdma-core/files/09-rdma.sh
+++ srcpkgs/rdma-core/files/09-rdma.sh
@@ -1,5 +1,4 @@
 # Load RDMA kernel modules at boot
-# Equivalent to systemd's rdma-load-modules@{rdma,infiniband,roce,iwarp}.service
 for conf in rdma infiniband roce iwarp; do
     [ -r "/etc/rdma/modules/${conf}.conf" ] || continue
     while read -r mod; do
